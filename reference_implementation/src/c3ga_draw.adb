4f2cc5f22ed721f46a542e100d9acd85683d3241
Roger Mc Murtrie <rogermc@grapevine.com.au>
2017-12-16 13:00:23 +1100

    Added draw points routine.

--- reference_implementation/src/c3ga_draw.adb
+++ reference_implementation/src/c3ga_draw.adb
@@ -20,7 +20,26 @@ package body C3GA_Draw is
 
    exception
       when anError :  others =>
-         Put_Line ("An exception occurred in C3GA_Draw.Draw 1.");
+         Put_Line ("An exception occurred in C3GA_Draw.Draw vector.");
+         raise;
+   end Draw;
+
+   --  -------------------------------------------------------------------------                Model_View_Matrix, Projection_Matrix : GL.Types.Singles.Matrix4;
+
+   procedure Draw (Render_Program : GL.Objects.Programs.Program;
+                   Model_View_Matrix, Projection_Matrix : GL.Types.Singles.Matrix4;
+                   aPoint : C3GA.Normalized_Point; Colour : GL.Types.Colors.Color;
+                   Scale: float := 1.0) is
+   begin
+      E3GA.Set_Coords (Vec_3D, C3GA.Get_Coord_1 (aVector),
+                       C3GA.Get_Coord_2 (aVector), 0.0);
+      E3GA.Set_Coords (Tail, 0.0, 0.0, 0.0);
+      GA_Draw.Draw_Vector (Render_Program, Model_View_Matrix, Projection_Matrix,
+                           Tail, Vec_3D, Colour, Scale);
+
+   exception
+      when anError :  others =>
+         Put_Line ("An exception occurred in C3GA_Draw.Draw point.");
          raise;
    end Draw;
 end C3GA_Draw;
--- reference_implementation/src/c3ga_draw.ads
+++ reference_implementation/src/c3ga_draw.ads
@@ -10,4 +10,8 @@ package C3GA_Draw is
                    Model_View_Matrix, Projection_Matrix : GL.Types.Singles.Matrix4;
                    aVector : C3GA.Vector_E3GA; Colour : GL.Types.Colors.Color;
                    Scale : float := 1.0);
+   procedure Draw (Render_Program : GL.Objects.Programs.Program;
+                   Model_View_Matrix, Projection_Matrix : GL.Types.Singles.Matrix4;
+                   aPoint : C3GA.Normalized_Point; Colour : GL.Types.Colors.Color;
+                   Scale: float := 1.0);
 end C3GA_Draw;
