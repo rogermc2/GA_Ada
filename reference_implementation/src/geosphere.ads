059cabdd72ba49ccd036bd9569688397fe4050df
Roger <rogermc@grapevine.com.au>
2017-12-18 08:49:43 +1100

    Changed to use Ada vectors.

--- reference_implementation/src/geosphere.ads
+++ reference_implementation/src/geosphere.ads
@@ -1,15 +1,22 @@
 
-with GL.Types;
+with Ada.Containers.Vectors;
 
 with E3GA;
 
 package Geosphere is
+
    --  some very ancient code to compute a triangulated sphere
 
-    type Geosphere (Max_Vertices : GL.Types.Int; Max_Faces : GL.Types.Int) is private;
-    type Geosphere_Face is private;
+   type Geosphere (Max_Vertices : integer; Max_Faces : integer) is private;
+   type Geosphere_Face is private;
 
-    procedure GS_Compute (Sphere : Geosphere; Depth : GL.Types.Int);
+   type Geosphere_Access is access Geosphere;
+
+   package Spherical_Vectors is new Ada.Containers.Vectors
+     (Element_Type => Geosphere_Access, Index_Type => Positive);
+   type S_Vector is new Spherical_Vectors.Vector with null record;
+
+    procedure GS_Compute (Sphere : Geosphere; Depth : integer);
 
 private
     type V_Array is array (1 .. 3) of integer;
@@ -29,13 +36,13 @@ private
         Contour_Visited   : Contour_Visited_Array;
     end record;
 
-   type Geosphere (Max_Vertices : GL.Types.Int; Max_Faces : GL.Types.Int) is record
-      Num_Vertices    : GL.Types.Int;
-      Num_Faces       : GL.Types.Int;
-      Num_Primitives  : GL.Types.Int;
-      Depth           : GL.Types.Int;
-      Vertices        : GL.Types.Singles.Vector3_Array (1 .. Max_Vertices);
-      Faces           : GL.Types.Singles.Vector3_Array (1 .. Max_Faces);
+   type Geosphere (Max_Vertices : integer; Max_Faces : integer) is record
+      Num_Vertices    : integer;
+      Num_Faces       : integer;
+      Num_Primitives  : integer;
+      Depth           : integer;
+      Vertices        : S_Vector;
+      Faces           : S_Vector;
    end record;
 
 end Geosphere;
