45d25f45d224db15029162b00dc906eef0899498
Roger Mc Murtrie <rogermc@grapevine.com.au>
2018-01-01 15:55:09 +1100

    Incorporated MV Simplify routine.

--- reference_implementation/src/multivector.adb
+++ reference_implementation/src/multivector.adb
@@ -2,6 +2,10 @@
 
 package body Multivector is
 
+   procedure Simplify (MV : in out Multivector)
+
+     --  -------------------------------------------------------------------------
+
    function Get_Basis_Vector (Index : Integer) return Multivector is
       BB : constant GA_Maths.Basis_Blade := New_Basis_Blade (Index);
       MV : Multivector;
@@ -18,7 +22,7 @@ package body Multivector is
       B1       : GA_Maths.Basis_Blade;
       B2       : GA_Maths.Basis_Blade;
       List_1   : Blade_List := MV1.Blades;
-      Cursor_1 : Cursor;
+      Cursor_1 : Cursor := List_1.First;
       MV       : Multivector;
    begin
       begin
@@ -26,7 +30,7 @@ package body Multivector is
             B1 := Element (Cursor_1);
             declare
                List_2   : Blade_List := MV1.Blades;
-               Cursor_2 : Cursor;
+               Cursor_2 : Cursor := List_2.First;
             begin
                while Has_Element (Cursor_2) loop
                   B2 := Element (Cursor_2);
@@ -35,8 +39,51 @@ package body Multivector is
             end;
          end loop;
       end;
+      Simplify (MV);
       return MV;
    end Outer_Product;
+
+   --  -------------------------------------------------------------------------
+
+   procedure Simplify (MV : in out Multivector) is
+      use Blade_List_Package;
+      Current      : GA_Maths.Basis_Blade;
+      Previous     : GA_Maths.Basis_Blade;
+      Has_Previous : Boolean := False;
+      New_MV       : Multivector;
+      Blades       : Blade_List := MV.Blades;
+      Blade_Cursor : Cursor := Blades.First;
+      Remove_Nulls :  Boolean := False;
+
+   begin
+      while Has_Element (Blade_Cursor) loop
+         Current := Element (Blade_Cursor);
+         if Blade_Scale (Current) = 0.0 then
+            Blades.Delete (Blade_Cursor);
+            Has_Previous := False;
+         elsif Has_Previous and then
+           Bitmap (Previous) = Bitmap (Current) then
+            Update_Blade (Previous, Blade_Scale (Previous) + Blade_Scale (Current));
+            Blades.Delete (Blade_Cursor);
+         else
+            if Has_Previous and then Blade_Scale (Previous) = 0.0 then
+               Remove_Nulls := True;
+            end if;
+            Previous := Current;
+         end if;
+      end loop;
+
+      Blade_Cursor := Blades.First;
+      if Remove_Nulls then
+         while Has_Element (Blade_Cursor) loop
+            Current := Element (Blade_Cursor);
+            if Blade_Scale (Current) = 0.0 then
+               Blades.Delete (Blade_Cursor);
+            end if;
+         end loop;
+      end if;
+      MV.Blades := Blades;
+   end Simplify;
 
    --  -------------------------------------------------------------------------
 
