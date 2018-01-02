18e348f36d03e12409d5b0caa36ba5b99072839f
Roger Mc Murtrie <rogermc@grapevine.com.au>
2018-01-02 18:39:39 +1100

    Blade routins moved to new Blade pckage.

---
+++ reference_implementation/src/blade.ads
@@ -0,0 +1,27 @@
+
+with Interfaces;
+
+with GA_Maths; use GA_Maths;
+
+package Blade is
+
+   type Basis_Blade is private;
+
+   function Bitmap (BB : Basis_Blade) return Unsigned_Integer;
+   function Blade_Scale (BB : Basis_Blade) return Float;
+   function Canonical_Reordering_Sign (Map_A, Map_B : Unsigned_Integer) return float;
+   function Outer_Product (BA, BB : Basis_Blade) return Basis_Blade;
+   function New_Basis_Blade (Index : Integer; Scale : Float := 1.0) return Basis_Blade;
+
+   procedure Update_Blade (BB : in out Basis_Blade; Scale : Float);
+   procedure Update_Blade (BB : in out Basis_Blade; Bitmap : Unsigned_Integer);
+   procedure Update_Blade (BB : in out Basis_Blade; Bitmap : Unsigned_Integer;
+                           Scale : Float);
+
+private
+   type Basis_Blade is record
+      Bitmap : Unsigned_Integer := 0;
+      Scale  : Float := 1.0;
+   end record;
+
+end Blade;
--- reference_implementation/src/ga_maths.ads
+++ reference_implementation/src/ga_maths.ads
@@ -22,8 +22,6 @@ package GA_Maths is
    type Fixed_4 is delta 0.01 range -1.0 .. 1.0;
    for Fixed_4'Small use 0.01;
 
-   type Basis_Blade is private;
-
    type Unsigned_Integer is mod 2 ** 32;
    US_1 : constant Unsigned_Integer := Unsigned_Integer (1);
    type Vector_Unsigned_3D is array (1 .. 3) of Interfaces.Unsigned_32;
@@ -70,22 +68,6 @@ package GA_Maths is
    GU_16    : constant Grade_Usage := 32;
 
    function Bit_Count (Bits : Unsigned_Integer) return Natural;
-   function Bitmap (BB : Basis_Blade) return Unsigned_Integer;
-   function Blade_Scale (BB : Basis_Blade) return Float;
-   function Canonical_Reordering_Sign (Map_A, Map_B : Unsigned_Integer) return float;
-   function Grade (Bits : Unsigned_Integer) return Natural;
-   function Outer_Product (BA, BB : Basis_Blade) return Basis_Blade;
-   function New_Basis_Blade (Index : Integer; Scale : Float := 1.0) return Basis_Blade;
-
-   procedure Update_Blade (BB : in out Basis_Blade; Scale : Float);
-   procedure Update_Blade (BB : in out Basis_Blade; Bitmap : Unsigned_Integer);
-   procedure Update_Blade (BB : in out Basis_Blade; Bitmap : Unsigned_Integer;
-                           Scale : Float);
-
-private
-   type Basis_Blade is record
-      Bitmap : Unsigned_Integer := 0;
-      Scale  : Float := 1.0;
-   end record;
+   function Grade (Bits : Unsigned_Integer) return Unsigned_Integer;
 
 end GA_Maths;
