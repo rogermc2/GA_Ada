947733f6669e0265f5789ea66525bac812db2630
Roger Mc Murtrie <rogermc@grapevine.com.au>
2018-01-03 10:34:31 +1100

    Changed scale to weight.

--- reference_implementation/src/blade.adb
+++ reference_implementation/src/blade.adb
@@ -10,10 +10,10 @@ package body Blade is
 
    --  ------------------------------------------------------------------------
 
-   function Blade_Scale (BB : Basis_Blade) return Float is
+   function Weight (BB : Basis_Blade) return Float is
    begin
-      return BB.Scale;
-   end Blade_Scale;
+      return BB.Weight;
+   end Weight;
 
    --  ------------------------------------------------------------------------
 
@@ -43,11 +43,11 @@ package body Blade is
       Sign      : Float;
    begin
       if Outer and ((BA.Bitmap and BB.Bitmap) /= 0) then
-         New_Blade.Scale := 0.0;
+         New_Blade.Weight := 0.0;
       else
          New_Blade.Bitmap := BA.Bitmap or BB.Bitmap;
          Sign := Canonical_Reordering_Sign (BA.Bitmap, BB.Bitmap);
-         New_Blade.Scale := Sign * BA.Scale * BB.Scale;
+         New_Blade.Weight := Sign * BA.Weight * BB.Weight;
       end if;
 
       return New_Blade;
@@ -62,20 +62,20 @@ package body Blade is
 
    --  ------------------------------------------------------------------------
 
-   function New_Basis_Blade (Index : Base; Scale : Float := 1.0) return Basis_Blade is
+   function New_Basis_Blade (Index : Base; Weight : Float := 1.0) return Basis_Blade is
       use Interfaces;
       Blade : Basis_Blade;
    begin
       Blade.Bitmap := Unsigned_Integer (Shift_Left (Unsigned_32 (1), Index'Enum_Rep));
-      Blade.Scale :=Scale;
+      Blade.Weight := Weight;
       return Blade;
    end New_Basis_Blade;
 
    --  ------------------------------------------------------------------------
 
-   procedure Update_Blade (BB : in out Basis_Blade; Scale : Float) is
+   procedure Update_Blade (BB : in out Basis_Blade; Weight : Float) is
    begin
-      BB.Scale := Scale;
+      BB.Weight := Weight;
    end Update_Blade;
 
    --  ------------------------------------------------------------------------
@@ -88,10 +88,10 @@ package body Blade is
    --  ------------------------------------------------------------------------
 
    procedure Update_Blade (BB : in out Basis_Blade; Bitmap : Unsigned_Integer;
-                           Scale : Float) is
+                           Weight : Float) is
    begin
       BB.Bitmap := Bitmap;
-      BB.Scale := Scale;
+      BB.Weight := Weight;
    end Update_Blade;
 
    --  ------------------------------------------------------------------------
--- reference_implementation/src/blade.ads
+++ reference_implementation/src/blade.ads
@@ -9,20 +9,20 @@ package Blade is
    type Base is (no, e1, e2, e3, ni);
 
    function Bitmap (BB : Basis_Blade) return Unsigned_Integer;
-   function Blade_Scale (BB : Basis_Blade) return Float;
+   function Weight (BB : Basis_Blade) return Float;
    function Canonical_Reordering_Sign (Map_A, Map_B : Unsigned_Integer) return float;
    function Outer_Product (BA, BB : Basis_Blade) return Basis_Blade;
-   function New_Basis_Blade (Index : Base; Scale : Float := 1.0) return Basis_Blade;
+   function New_Basis_Blade (Index : Base; Weight : Float := 1.0) return Basis_Blade;
 
-   procedure Update_Blade (BB : in out Basis_Blade; Scale : Float);
+   procedure Update_Blade (BB : in out Basis_Blade; Weight : Float);
    procedure Update_Blade (BB : in out Basis_Blade; Bitmap : Unsigned_Integer);
    procedure Update_Blade (BB : in out Basis_Blade; Bitmap : Unsigned_Integer;
-                           Scale : Float);
+                           Weight : Float);
 
 private
    type Basis_Blade is record
       Bitmap : Unsigned_Integer := 0;
-      Scale  : Float := 1.0;
+      Weight : Float := 1.0;
    end record;
 
 end Blade;
