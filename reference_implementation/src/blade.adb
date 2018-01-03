1fb8bbcac852c80064a0d5ed9d86a39aab3dfc32
Roger Mc Murtrie <rogermc@grapevine.com.au>
2018-01-03 18:59:16 +1100

    Added Inner_Product and other routines.

--- reference_implementation/src/blade.adb
+++ reference_implementation/src/blade.adb
@@ -4,6 +4,9 @@ with Interfaces;
 package body Blade is
 
    function GP_OP (BA, BB : Basis_Blade; Outer : Boolean) return Basis_Blade;
+   function Inner_Product_Filter (Grade_1, Grade_2 : Unsigned_Integer;
+                                  BB : Basis_Blade; Cont : Contraction_Type)
+                                  return Basis_Blade;
 
    --  ------------------------------------------------------------------------
 
@@ -67,12 +70,38 @@ package body Blade is
 
    --  ------------------------------------------------------------------------
 
+   function Grade (BB : Basis_Blade) return Unsigned_Integer is
+   begin
+      return Unsigned_Integer (GA_Maths.Bit_Count (BB.Bitmap));
+   end Grade;
+
+   --  ------------------------------------------------------------------------
+
    function Grade_Inversion (B : Basis_Blade) return Basis_Blade is
       W : constant float
         := Float (Minus_1_Power (Grade (B.Bitmap)) * Unsigned_Integer (B.Weight));
    begin
       return New_Basis_Blade (B.Bitmap, W);
    end Grade_Inversion;
+
+   --  ------------------------------------------------------------------------
+
+   function Inner_Product (BA, BB : Basis_Blade; Cont : Contraction_Type)
+                           return Basis_Blade is
+   begin
+      return Inner_Product_Filter (Grade (BA), Grade (BB),
+                                   Geometric_Product (BA, BB), Cont);
+   end Inner_Product;
+
+   --  ------------------------------------------------------------------------
+
+   function Inner_Product_Filter (Grade_1, Grade_2 : Unsigned_Integer;
+                                  BB : Basis_Blade; Cont : Contraction_Type)
+                                  return Basis_Blade is
+      IP_Blade : Basis_Blade;
+   begin
+      return IP_Blade;
+   end Inner_Product_Filter;
 
    --  ------------------------------------------------------------------------
 
