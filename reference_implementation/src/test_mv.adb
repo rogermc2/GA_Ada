a695e6a2fd72e76e93bb68804074f94690b2bdae
Roger <rogermc@grapevine.com.au>
2018-01-04 10:44:31 +1100

    New project MV Test

---
+++ reference_implementation/src/test_mv.adb
@@ -0,0 +1,11 @@
+
+with Ada.Text_IO; use Ada.Text_IO;
+
+with Multivector; use Multivector;
+
+procedure Test_Mv is
+   MV : Multivector.Multivector;
+begin
+   --  Insert code here.
+   null;
+end Test_Mv;
