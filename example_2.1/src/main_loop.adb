5bb37d79faaa892c3baad9900d734cf3e2128212
Roger Mc Murtrie <rogermc@grapevine.com.au>
2018-01-07 15:56:08 +1100

    Updated to latest version.

--- example_2.1/src/main_loop.adb
+++ example_2.1/src/main_loop.adb
@@ -40,7 +40,7 @@ with E3GA_Utilities;
 with GA_Maths;
 
 with Silo;
---  with Texture_Management;
+
 with Text_Management;
 
 procedure Main_Loop (Main_Window : in out Glfw.Windows.Window) is
@@ -55,10 +55,10 @@ procedure Main_Loop (Main_Window : in out Glfw.Windows.Window) is
    Key_Pressed    : boolean := False;
    Parallelogram  : boolean := True;
 
-   procedure Draw_Parallelogram (Render_Program          : GL.Objects.Programs.Program;
-                                 MV_Matrix, Proj_Matrix  : GL.Types.Singles.Matrix4;
-                                 V2, V3, V4              : E2GA.Vector;
-                                 Colour                  : GL.Types.Colors.Color);
+   procedure Draw_Parallelogram (Render_Program : GL.Objects.Programs.Program;
+                                 MV_Matrix      : GL.Types.Singles.Matrix4;
+                                 V2, V3, V4     : E2GA.Vector;
+                                 Colour         : GL.Types.Colors.Color);
    procedure Draw_Text (Window_Width, Window_Height : Glfw.Size;
                         theText         : String;
                         Render_Program  : GL.Objects.Programs.Program;
@@ -86,7 +86,7 @@ procedure Main_Loop (Main_Window : in out Glfw.Windows.Window) is
       Label             : Silo.Label_Data;
       Label_Position    : GL.Types.Singles.Vector2;
       --  How many bivectors? what spacing between them?:
-      Entry_Width       : constant single := 2.3;
+      Entry_Width       : constant single := 2.8;
       Entry_Height      : constant single := 3.5;
       Num_Bivector_X    : constant integer := 6;
       Num_Bivector_Y    : constant integer := 4;
@@ -94,7 +94,7 @@ procedure Main_Loop (Main_Window : in out Glfw.Windows.Window) is
       Scale_S           : constant single := single (Scale);
       Text_Scale        : constant single := 0.12;
       Position_X        : integer := 0;
-      Position_Y        : single := 160.0;
+      Position_Y        : single := 100.0;
 
       A                 : float := 0.0;
       BV                : E2GA.Bivector;
@@ -125,15 +125,12 @@ procedure Main_Loop (Main_Window : in out Glfw.Windows.Window) is
                               GL.Types.Int (Window_Height));
       Utilities.Clear_Background_Colour_And_Depth (Back_Colour);
 
-      Maths.Init_Orthographic_Transform (0.0, Single (Window_Width),
-                                         0.0, Single (Window_Height),
-                                         -100.0, 100.0, Projection_Matrix);
       --  Set scale and position of first diagram
-      Translation_Matrix := Maths.Translation_Matrix ((Entry_Width * Scale_S / 2.0,
-                                                     (Single (Num_Bivector_Y)) * Entry_Height * Scale_S / 2.0
-                                                     - Position_Y, 0.0));
-      Model_View_Matrix := Maths.Scaling_Matrix ((Scale_S, Scale_S, Scale_S));
-
+      Translation_Matrix := Maths.Translation_Matrix
+        ((Entry_Width * Scale_S / 2.0,
+          (Single (Num_Bivector_Y)) * Entry_Height * Scale_S / 2.0 - Position_Y, 0.0));
+     Model_View_Matrix := Maths.Scaling_Matrix ((Scale_S, Scale_S, Scale_S));
+      GA_Draw.Set_Projection_Matrix (Projection_Matrix);
       --  The final MVP matrix is set up in the draw routines
       Set_Coords (V1, E11, E12);
       while A < Two_Pi - 0.1 loop
@@ -142,20 +139,19 @@ procedure Main_Loop (Main_Window : in out Glfw.Windows.Window) is
                      Cos (A) * E21 - Sin (A) * E22);
          Model_View_Matrix := Translation_Matrix * Model_View_Matrix;
          E2GA_Draw.Draw (Render_Graphic_Program, Model_View_Matrix,
-                         Projection_Matrix, V1, Red, Scale);
+                         V1, Red, Scale);
          E2GA_Draw.Draw (Render_Graphic_Program, Model_View_Matrix,
-                         Projection_Matrix, V2, Green, Scale);
+                         V2, Green, Scale);
 
          BV := E2GA.Outer_Product (V1, V2);
          if Parallelogram then
             --  Draw Quad with vertices: origin -> V1 -> V1+V2 -> V2
             Draw_Parallelogram (Render_Graphic_Program, Model_View_Matrix,
-                                Projection_Matrix, V1, V1 + V2, V2, Blue);
+                                V1, V1 + V2, V2, Blue);
          else
-            null;
             BV_Translation_Matrix := Translation_Matrix * BV_Translation_Matrix;
             E2GA_Draw.Draw (Render_Graphic_Program, BV_Translation_Matrix,
-                            Projection_Matrix, BV, Yellow);
+                            BV, Yellow);
          end if;
 
          if A < Pi - 0.1 then
@@ -171,7 +167,6 @@ procedure Main_Loop (Main_Window : in out Glfw.Windows.Window) is
          Label := Silo.Set_Data (Ada.Strings.Unbounded.To_Unbounded_String
                                  (E2GA.Bivector_String
                                     (BV)), Label_Position);
-         --         Label := Silo.Set_Data (Ada.Strings.Unbounded.To_Unbounded_String ("Hello"), Label_Position);
          Silo.Push (Label);
 
          --  Set X position of next diagram
@@ -205,15 +200,16 @@ procedure Main_Loop (Main_Window : in out Glfw.Windows.Window) is
 
    --  ------------------------------------------------------------------------
 
-   procedure Draw_Parallelogram (Render_Program          : GL.Objects.Programs.Program;
-                                 MV_Matrix, Proj_Matrix  : GL.Types.Singles.Matrix4;
-                                 V2, V3, V4              : E2GA.Vector;
-                                 Colour                  : GL.Types.Colors.Color) is
+   procedure Draw_Parallelogram (Render_Program  : GL.Objects.Programs.Program;
+                                 MV_Matrix       : GL.Types.Singles.Matrix4;
+                                 V2, V3, V4      : E2GA.Vector;
+                                 Colour          : GL.Types.Colors.Color) is
       use GL.Objects.Buffers;
       use GL.Types.Colors;
       Colour_Location      : GL.Uniforms.Uniform;
       MV_Matrix_ID         : GL.Uniforms.Uniform;
       Projection_Matrix_ID : GL.Uniforms.Uniform;
+      Projection_Matrix    : GL.Types.Singles.Matrix4;
       Vertex_Array_Object  : GL.Objects.Vertex_Arrays.Vertex_Array_Object;
       Vertex_Buffer        : Buffer;
       Element_Buffer       : Buffer;
@@ -229,9 +225,11 @@ procedure Main_Loop (Main_Window : in out Glfw.Windows.Window) is
       Vertex_Array_Object.Initialize_Id;
       Vertex_Array_Object.Bind;
 
+      GA_Draw.Set_Projection_Matrix (Projection_Matrix );
+
       GA_Draw.Graphic_Shader_Locations (Render_Program, MV_Matrix_ID, Projection_Matrix_ID,
                                 Colour_Location);
-      GL.Uniforms.Set_Single (Projection_Matrix_ID, Proj_Matrix);
+      GL.Uniforms.Set_Single (Projection_Matrix_ID, Projection_Matrix);
 
       Vertex_Buffer.Initialize_Id;
       Array_Buffer.Bind (Vertex_Buffer);
@@ -243,7 +241,7 @@ procedure Main_Loop (Main_Window : in out Glfw.Windows.Window) is
 
       GL.Uniforms.Set_Single (Colour_Location, Colour (R), Colour (G), Colour (B));
       GL.Uniforms.Set_Single (MV_Matrix_ID, MV_Matrix);
-      GL.Uniforms.Set_Single (Projection_Matrix_ID, Proj_Matrix);
+      GL.Uniforms.Set_Single (Projection_Matrix_ID, Projection_Matrix);
 
       GL.Attributes.Set_Vertex_Attrib_Pointer (0, 3, Single_Type, 0, 0);
       GL.Attributes.Enable_Vertex_Attrib_Array (0);
