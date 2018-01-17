f67f460620c1d565c14f2cd76d33170da3a34126
Roger Mc Murtrie <rogermc@grapevine.com.au>
2018-01-17 17:19:02 +1100

    Initial conversion complete.

--- reference_implementation/src/e3ga_utilities.adb
+++ reference_implementation/src/e3ga_utilities.adb
@@ -127,49 +127,49 @@ package body E3GA_Utilities is
    --                                = bx        + i by  (with respect to a)
    --  for theta = angle from a to b.
    function Rotor_Vector_To_Vector (V_From, V_To : Multivector.Vector) return Multivector.Rotor is
-      use Interfaces;
-      use GA_Maths;
       use GA_Maths.Float_Functions;
       use E3GA;
       use Multivector;
-      V1     : Vector_Unsigned := To_Unsigned (V_From);
-      V2     : Vector_Unsigned := To_Unsigned (V_To);
       C1     : float;
       S      : float;
       w0     : Vector;
       w1     : Vector;
       w2     : Vector;
       N2     : Float;
-      R      : Rotor;
+      R      : Float;
       Result : Rotor;
    begin
       Set_Coords (w0, 0.0, 0.0, 0.0);
       Set_Coords (w1, 0.0, 0.0, 0.0);
       Set_Coords (w2, 0.0, 0.0, 0.0);
-      if float (E3GA.Get_Coord (Scalar_Product (V_From, V_To))) < -0.9 then
+--        if float (E3GA.Get_Coord (Scalar_Product (V_From, V_To))) < -0.9 then
+--           C1 := E3GA.Get_Coord_1 (Left_Contraction (V_From, Outer_Product (V_From, V_To)));
+      if  Scalar_Product (V_From, V_To) < -0.9 then
          C1 := E3GA.Get_Coord_1 (Left_Contraction (V_From, Outer_Product (V_From, V_To)));
          Set_Coords (w0, C1, 0.0, 0.0);
          N2 := Norm_E2 (w0);
 
-         if E3GA.Get_Coord (N2) = 0.0 then
+         if N2 = 0.0 then
             C1 := E3GA.Get_Coord_1 (Left_Contraction (V_From, Outer_Product (V_From, e1)));
             Set_Coords (w1, C1, 0.0, 0.0);
             C1 := E3GA.Get_Coord_1 (Left_Contraction (V_From, Outer_Product (V_From, e2)));
             Set_Coords (w2, C1, 0.0, 0.0);
-            if E3GA.Get_Coord (Norm_E2 (w1)) > E3GA.Get_Coord (Norm_E2 (w2)) then
-               Set_Rotor (Result, Outer_Product (V_From, Unit_e (w1)));
+            if Norm_E2 (w1) > Norm_E2 (w2) then
+--                 Set_Rotor (Result, Outer_Product (V_From, Unit_e (w1)));
+               Result := Outer_Product (V_From, Unit_e (w1));
             else
-               Set_Rotor (Result, Outer_Product (V_From, Unit_e (w2)));
+               Result := Outer_Product (V_From, Unit_e (w2));
             end if;
-         else
+         else  --  N2 /= 0.0
             --  Replace V1 with -V1 and additional 180 degree rotation.
-            S := Sqrt (2.0 * float (1.0 - Dot_Product (V_To, V_From)));
-            R := (1.0 - Geometric_Product (V_To, V_From)) / S;
+            S := Sqrt (2.0 * float (1.0 - Scalar_Part (Left_Contraction (V_To, V_From))));
+            R := (1.0 - Scalar_Part (Geometric_Product (V_To, V_From))) / S;
             Result := Geometric_Product (R, Outer_Product (V_From, Unit_e (w0)));
          end if;
       else
-         S := Sqrt (2.0 * float (1.0 + Dot_Product (V_To, V_From)));
-         Result := (1.0 + Geometric_Product (V_To, V_From)) / S;
+         S := Sqrt (2.0 * float (1.0 + Scalar_Part (Left_Contraction (V_To, V_From))));
+         R := (1.0 + Scalar_Part (Geometric_Product (V_To, V_From))) / S;
+         Result := New_Rotor (R);
       end if;
       return Result;
    exception
