b33ec8f3b743223b42c6422ccaeabe6be2ae3bb6
Roger Mc Murtrie <rogermc@grapevine.com.au>
2018-02-04 17:41:36 +1100

    Added Get Matrix function

--- reference_implementation/src/metric.adb
+++ reference_implementation/src/metric.adb
@@ -5,23 +5,69 @@ package body Metric is
 
    type Metric_Array is array (Integer range <>) of float;
 
-   function New_Metric (Dimension : Integer) return Metric is
-      theMetric : Metric (1 .. Dimension, 1 .. Dimension) :=
-      (others => (others => 0.0));
+   function Matrix (Met : Metric_Record) return GA_Maths.Float_Matrix is
+   begin
+      return Met.Matrix;
+   end Matrix;
+
+   --  --------------------------------------------------------------------
+
+   function New_Metric (Dimension : Integer) return Metric_Matrix is
+      theMetric : Metric_Matrix (1 .. Dimension, 1 .. Dimension) :=
+        (others => (others => 0.0));
    begin
       return theMetric;
    end New_Metric;
 
    --  ------------------------------------------------------------------------
 
-   function New_Metric (Dimension : Integer; Data : Metric_Data) return Metric is
-      theMetric : Metric (1 .. Dimension, 1 .. Dimension) :=
-      (others => (others => 0.0));
+   function New_Metric (Dimension : Integer; Data : Metric_Data) return Metric_Matrix is
+      theMetric : Metric_Matrix (1 .. Dimension, 1 .. Dimension) :=
+        (others => (others => 0.0));
    begin
       for Index in 1 .. Dimension loop
          theMetric (Index, Index) := Data (Index);
       end loop;
       return theMetric;
+   end New_Metric;
+
+   --  ------------------------------------------------------------------------
+
+   function New_Metric (Met : Metric_Matrix) return Metric_Record is
+      use GA_Maths;
+      use Float_Array_Package;
+      Eigen_Values  : Real_Vector (Met'Range);
+      Eigin_Vectors : Float_Matrix (Met'Range, Met'Range);
+      theMetric     : Metric_Record (Met'Last - Met'First + 1);
+   begin
+      for row in Met'Range loop
+         for col in Met'Range(2) loop
+            theMetric.Matrix (row,col) := Met (row,col);
+         end loop;
+      end loop;
+      Eigensystem (theMetric.Matrix, Eigen_Values, Eigin_Vectors);
+      theMetric.Eigen_Metric := Eigen_Values;
+      theMetric.Diagonal := Is_Diagonal (theMetric.Matrix);
+
+      if theMetric.Diagonal then
+         theMetric.Euclidean := True;
+         theMetric.Anti_Euclidean := True;
+         for col in Met'Range loop
+            if  theMetric.Matrix (col, col) /= 1.0 then
+               theMetric.Euclidean := False;
+            end if;
+            if  theMetric.Matrix (col, col) /= -1.0 then
+               theMetric.Anti_Euclidean := False;
+            end if;
+         end loop;
+      end if;
+      return theMetric;
+
+   exception
+      when anError :  others =>
+         Put_Line ("An exception occurred in Metric.New_Metric 3");
+         raise;
+
    end New_Metric;
 
    --  ------------------------------------------------------------------------
--- reference_implementation/src/metric.ads
+++ reference_implementation/src/metric.ads
@@ -1,12 +1,29 @@
 
+with Ada.Exceptions;
+
+with GA_Maths;
+
 package Metric is
 
-   type Metric is array (Integer range <>, Integer range <>) of float;
+   type Metric_Record (Dim : Integer) is private;
+   type Metric_Matrix is array (Integer range <>, Integer range <>) of float;
    type Metric_Data is array (Integer range <>) of float;
 
-   Null_Metric : constant Metric (1 .. 0, 1 .. 0) := (others => (others => 0.0));
+   Null_Metric : constant Metric_Matrix (1 .. 0, 1 .. 0) := (others => (others => 0.0));
+   Metric_Exception : Ada.Exceptions.Exception_Id;
 
-   function New_Metric (Dimension : Integer) return Metric;
-   function New_Metric (Dimension : Integer; Data : Metric_Data) return Metric;
+   function Matrix (Met : Metric_Record) return GA_Maths.Float_Matrix;
+   function New_Metric (Dimension : Integer) return Metric_Matrix;
+   function New_Metric (Dimension : Integer; Data : Metric_Data) return Metric_Matrix;
+   function New_Metric (Met : Metric_Matrix) return Metric_Record;
+
+private
+   type Metric_Record (Dim : Integer) is record
+      Matrix          : GA_Maths.Float_Matrix (1 .. Dim, 1 .. Dim);
+      Eigen_Metric    : GA_Maths.Float_Array_Package.Real_Vector (1 .. Dim);
+      Diagonal        : Boolean;
+      Euclidean       : Boolean := False;
+      Anti_Euclidean  : Boolean := False;
+   end record;
 
 end Metric;
