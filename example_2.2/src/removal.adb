feb9a3cea7c77a4cfad04b13a16178510ccc1f68
Roger Mc Murtrie <rogermc@grapevine.com.au>
2018-02-22 12:21:38 +1100

    Style corrrections

--- example_2.2/src/removal.adb
+++ example_2.2/src/removal.adb
@@ -13,21 +13,22 @@ with Initialize;
 with Main_Loop;
 
 procedure Removal is
-    Main_Window : Glfw.Windows.Window;
-    Window_Title : String := "GA for Computer Scientists Chapter 2, Example 2: Hidden Surface Removal";
+   Main_Window : Glfw.Windows.Window;
+   Window_Title : String :=
+     "GA for Computer Scientists Chapter 2, Example 2: Hidden Surface Removal";
 begin
-    Glfw.Init;
-    Initialize (Main_Window, Window_Title);
-    Main_Loop (Main_Window);
-    Glfw.Shutdown;
+   Glfw.Init;
+   Initialize (Main_Window, Window_Title);
+   Main_Loop (Main_Window);
+   Glfw.Shutdown;
 
 exception
-    when anError : Constraint_Error =>
-        Put ("Removal returned constraint error: ");
-        Put_Line (Exception_Information (anError));
+   when anError : Constraint_Error =>
+      Put ("Removal returned constraint error: ");
+      Put_Line (Exception_Information (anError));
 
-    when anError :  others =>
-        Put_Line ("An exception occurred in Removal.");
-        Put_Line (Exception_Information (anError));
+   when anError :  others =>
+      Put_Line ("An exception occurred in Removal.");
+      Put_Line (Exception_Information (anError));
 
 end Removal;
