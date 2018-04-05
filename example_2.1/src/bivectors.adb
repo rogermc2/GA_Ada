704ffd9a86f62f7c86d97bf8663c0abb8507d878
Roger Mc Murtrie <rogermc@grapevine.com.au>
2018-04-05 12:08:31 +1000

    Style fixes

--- example_2.1/src/bivectors.adb
+++ example_2.1/src/bivectors.adb
@@ -12,21 +12,21 @@ with Initialize;
 with Main_Loop;
 
 procedure Bivectors is
-    Main_Window : Glfw.Windows.Window;
-    Window_Title : String := "GA for Computer Scientists Draw Bivectors Example 2.1";
+   Main_Window : Glfw.Windows.Window;
+   Window_Title : String := "GA for Computer Scientists Draw Bivectors Example 2.1";
 begin
-    Glfw.Init;
-    Initialize (Main_Window, Window_Title);
-    Main_Loop (Main_Window);
-    Glfw.Shutdown;
+   Glfw.Init;
+   Initialize (Main_Window, Window_Title);
+   Main_Loop (Main_Window);
+   Glfw.Shutdown;
 
 exception
-    when anError : Constraint_Error =>
-        Put ("Bivectors returned constraint error: ");
-        Put_Line (Exception_Information (anError));
+   when anError : Constraint_Error =>
+      Put ("Bivectors returned constraint error: ");
+      Put_Line (Exception_Information (anError));
 
-    when anError :  others =>
-        Put_Line ("An exception occurred in Bivectors.");
-        Put_Line (Exception_Information (anError));
+   when anError :  others =>
+      Put_Line ("An exception occurred in Bivectors.");
+      Put_Line (Exception_Information (anError));
 
 end Bivectors;
