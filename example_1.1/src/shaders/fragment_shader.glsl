0beddd666614ac02d180db014853977845ed2ad7
rogermc2 <rogermc@iinet.net.au>
2019-11-29 17:57:18 +1100

    Refined shaders

--- example_1.1/src/shaders/fragment_shader.glsl
+++ example_1.1/src/shaders/fragment_shader.glsl
@@ -1,10 +1,50 @@
 #version 410 core
 
-uniform vec3 vector_colour;
+//  Based on OGL_Turorials Tutorial 08_basic_shading
+// Interpolated values from the vertex shaders
+in vec3 Position_Worldspace;
+in vec3 Camera_Normal;
+in vec3 Eye_Direction;
+in vec3 Light_Direction;
 
-out vec3 fragment_colour;
+// Ouput data
+out vec3 color;
+
+// Values that stay constant for the whole mesh.
+uniform sampler2D myTextureSampler;
+uniform mat4 MV;
+uniform vec3 Light_Position_Worldspace;
 
 void main()
 {
-   fragment_colour = vector_colour;
+    // Light emission properties
+    // You probably want to put them as uniforms
+    vec3 LightColor = vec3(1,1,1);
+    float LightPower = 50.0f;
+    
+    // Distance to the light
+    float distance = length( Light_Position_Worldspace - Position_Worldspace );
+    
+    // Normal of the computed fragment, in camera space
+    vec3 norm = normalize(Camera_Normal);
+    // Direction of the light (from the fragment to the light)
+    vec3 light_dir = normalize(Light_Direction);
+    // Cosine of the angle between the normal and the light direction,
+    // clamped above 0
+    //  - light is at the vertical of the triangle -> 1
+    //  - light is perpendicular to the triangle -> 0
+    //  - light is behind the triangle -> 0
+    float cosTheta = clamp(dot(norm, light_dir), 0, 1);
+    
+    // Eye vector (towards the camera)
+    vec3 Eye_Dir = normalize(Eye_Direction);
+    // Direction in which the triangle reflects the light
+    vec3 Refl = reflect(-light_dir, norm);
+    // Cosine of the angle between the Eye vector and the Reflect vector,
+    // clamped to 0
+    //  - Looking into the reflection -> 1
+    //  - Looking elsewhere -> < 1
+    float cosAlpha = clamp( dot(Eye_Dir, Refl), 0,1 );
+    
+    color = LightColor * LightPower * cosTheta / (distance*distance);
 }
--- example_1.1/src/shaders/vertex_shader.glsl
+++ example_1.1/src/shaders/vertex_shader.glsl
@@ -1,11 +1,33 @@
 #version 410 core
 
 layout(location = 0) in vec3 vertex_position;
+layout(location = 1) in vec3 vertex_normal;
 
-uniform mat4 MV_Matrix;
-uniform mat4 Proj_Matrix;
+out vec3 Position_Worldspace;
+out vec3 Camera_Normal;
+out vec3 Eye_Direction;
+out vec3 Light_Direction;
+
+uniform mat4 view_matrix;
+uniform mat4 model_matrix;
+uniform mat4 mv_matrix;
+uniform mat4 projection_matrix;
+uniform vec3 light_position;
 
 void main()
-{
-  gl_Position = Proj_Matrix * MV_Matrix * vec4(vertex_position, 1);
-}
+  {
+   gl_Position = projection_matrix * mv_matrix * vec4(vertex_position, 1);
+      Position_Worldspace = (model_matrix * vec4(vertex_position, 1)).xyz;
+      
+      // Vector that goes from the vertex to the camera, in camera space.
+      // In camera space, the camera is at the origin (0,0,0).
+      vec3 Vertex_Position_Camera_Space = (view_matrix * model_matrix * vec4(vertex_position,1)).xyz;
+      Eye_Direction = vec3(0, 0, 0) - Vertex_Position_Camera_Space;
+      // Vector that goes from the vertex to the light, in camera space. M is ommited because it's identity.
+      vec3 LightPosition = (view_matrix * vec4(light_position, 1)).xyz;
+      Light_Direction = Light_Direction + Eye_Direction;
+
+   Camera_Normal = (view_matrix * model_matrix * vec4(vertex_normal, 0)).xyz;
+    // Only correct if Model_Matrix does not scale the model !
+    // Use its inverse transpose if not.
+  }
