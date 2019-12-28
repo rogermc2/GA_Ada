bef67eb48eb77118a0cb3907449797a430c1a48d
rogermc2 <rogermc@iinet.net.au>
2019-12-28 11:43:33 +1100

    Metric_C3 matrix declared.

--- ga_lib/src/metric.ads
+++ ga_lib/src/metric.ads
@@ -9,8 +9,14 @@ package Metric is
    type Metric_Matrix is array (Integer range <>, Integer range <>) of float;
    type Metric_Data is array (Integer range <>) of float;
 
-   Null_Metric : constant Metric_Matrix (1 .. 0, 1 .. 0) := (others => (others => 0.0));
+   Null_Metric      : constant Metric_Matrix (1 .. 0, 1 .. 0) := (others => (others => 0.0));
    Metric_Exception : Ada.Exceptions.Exception_Id;
+   Metric_C3        : constant Metric_Matrix (1 .. 5, 1 .. 5) :=
+                         (1 => (5  => -1.0, others => 0.0),
+                          2 => (2  => 1.0, others => 0.0),
+                          3 => (3  => 1.0, others => 0.0),
+                          4 => (4  => 1.0, others => 0.0),
+                          5 => (1  => -1.0, others => 0.0));
 
    function Matrix (Met : Metric_Record) return GA_Maths.Float_Matrix;
    function New_Metric (Dimension : Integer) return Metric_Matrix;
