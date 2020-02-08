00cd839625952aab3ec8ab674febcc3f0d6f5132
rogermc2 <rogermc@iinet.net.au>
2020-02-08 21:16:58 +1100

    Coding

--- ga_ref_impl/src/inner_product_types.adb
+++ ga_ref_impl/src/inner_product_types.adb
@@ -5,6 +5,7 @@ with Interfaces;
 
 with Maths;
 
+with Bits;
 with Blade;
 with Blade_Types;
 with GA_Maths;
@@ -14,10 +15,10 @@ package body Inner_Product_Types is
 
     type Scale_Array is array (Integer range <>) of Float;
 
-    function Factorize_Blade (B : Multivectors.Multivector;
+    function Factorize_Blade (MV_B : Multivectors.Multivector;
                               Scale : in out Scale_Array)
                               return Multivectors.Multivector_List;
-    function Factorize_Blade_Fast (B : Multivectors.Multivector; Scale : in out Scale_Array)
+    function Factorize_Blade_Fast (MV_B : Multivectors.Multivector; Scale : in out Scale_Array)
                                    return Multivectors.Multivector_List;
 
     --  --------------------------------------------------------------------
@@ -46,6 +47,9 @@ package body Inner_Product_Types is
         MV_Fast      : Multivector;
         Factors      : Multivector_List;
         Fast_Factors : Multivector_List;
+        K            : Integer;
+        OK           : Boolean;
+        Check_Scale  : Float;
     begin
         for index in 1 .. dim loop
             MV_B := Random_Blade
@@ -56,21 +60,42 @@ package body Inner_Product_Types is
             Blades.Append (New_Basis_Blade (23,  0.218277));
             Blades.Append (New_Basis_Blade (15, -0.213881));
             Update (MV_B, Blades);
+
             Factors := Factorize_Blade (MV_B, Scales);
             MV_R := New_Multivector (0.0);
-
             for index in 1 .. List_Length (Factors) loop
                 MV_R := Outer_Product (MV_R, MV_Item (Factors, index));
             end loop;
-            MV_Fast := New_Multivector (0.0);
+
             Fast_Factors := Factorize_Blade_Fast (MV_B, Scales);
+            MV_Fast := New_Multivector (0.0);
+            for index in 1 .. List_Length (Fast_Factors) loop
+                MV_Fast := Outer_Product (MV_Fast, MV_Item (Fast_Factors, index));
+            end loop;
+
+            MV_B := Unit_E (MV_B);
+            MV_R := Unit_E (MV_R);
+            MV_Fast := Unit_E (MV_Fast);
+
+            OK := Grade (MV_B, K);
+            if not OK then
+                raise Inner_Product_Types_Exception with
+                  "Inner_Product_Types.Init_Conformal, empty multivector detected.";
+            else
+                Check_Scale := Scalar_Part (Geometric_Product (MV_R, Versor_Inverse (MV_Fast)));
+                if Check_Scale < 0.0 then
+                    raise Inner_Product_Types_Exception with
+                      "Inner_Product_Types.Init_Conformal, multivector Scalar_Part < 0.";
+                end if;
+            end if;
+
         end loop;
         return  C3_M;
     end Init_Conformal;
 
     --  --------------------------------------------------------------------
 
-    function Factorize_Blade (B : Multivectors.Multivector; Scale : in out Scale_Array)
+    function Factorize_Blade (MV_B : Multivectors.Multivector; Scale : in out Scale_Array)
                              return Multivectors.Multivector_List is
         use Interfaces;
         use Blade;
@@ -80,22 +105,22 @@ package body Inner_Product_Types is
         K         : Integer;
         S         : Float;
         E         : Basis_Blade;
-        E_Array   : array (0 .. Space_Dimension (B)) of Basis_Blade;
+        E_Array   : array (0 .. Space_Dimension (MV_B)) of Basis_Blade;
         Idx       : Integer := 0;
         Basis_Bit : Unsigned_32;
         B_Current : Multivector;
         aFactor   : Multivector;
         Factors   : Multivector_List;
-        OK        : constant Boolean := Grade (B, K);
+        OK        : constant Boolean := Grade (MV_B, K);
     begin
         if not OK then
             raise Inner_Product_Types_Exception with
               "Inner_Product_Types.Factorize_Blade Empty multivector detected.";
         else
             if K = 0 then
-                S := Scalar_Part (B);
+                S := Scalar_Part (MV_B);
             else
-                S := Norm_E (B);
+                S := Norm_E (MV_B);
             end if;
 
             if Scale'length > 0 then
@@ -103,10 +128,10 @@ package body Inner_Product_Types is
             end if;
 
             if K > 0 and S /= 0.0 then
-                E := Largest_Basis_Blade (B);
+                E := Largest_Basis_Blade (MV_B);
                 E_Array (0) := New_Basis_Blade (C3_Base'Enum_Val (K));
 
-                for Index_G in Unsigned_32 range 0 .. Unsigned_32 (Space_Dimension (B) - 1) loop
+                for Index_G in Unsigned_32 range 0 .. Unsigned_32 (Space_Dimension (MV_B) - 1) loop
                     Basis_Bit := Shift_Left (1, Integer (Index_G));
                     if Bitmap (E) > 0 and Basis_Bit /= 0 then
                         Idx := Idx + 1;
@@ -141,34 +166,36 @@ package body Inner_Product_Types is
 
     --  --------------------------------------------------------------------
 
-    function Factorize_Blade_Fast (B : Multivectors.Multivector; Scale : in out Scale_Array)
+    function Factorize_Blade_Fast (MV_B : Multivectors.Multivector; Scale : in out Scale_Array)
                                    return Multivectors.Multivector_List is
         use Interfaces;
         use Blade;
         use Blade_Types;
         use GA_Maths;
         use Multivectors;
-        K           : Integer;
-        S           : Float;
-        E           : Basis_Blade;
-        E_Array     : array (0 .. Space_Dimension (B)) of Basis_Blade;
-        Lowest_Bit  : Integer;
-        Highrdt_Bit : Integer;
-        Idx         : Integer := 0;
-        Basis_Bit   : Unsigned_32;
-        B_Current   : Multivector;
-        aFactor     : Multivector;
-        Factors     : Multivector_List;
-        OK          : constant Boolean := Grade (B, K);
+        K            : Integer;
+        S            : Float;
+        Sc           : Float;
+        E            : Basis_Blade;
+        Lowest_Bit   : Integer;
+        Highest_Bit  : Integer;
+        Blades_B     : Blade_List;
+        Basis_Bit    : Unsigned_32;
+        Basis_Bitmap : Unsigned_32;
+        Vec_Bitmap   : Unsigned_32;
+        Blades_Bj    : Basis_Blade;
+        Factors      : Multivector_List;  --  F
+        L_List       : Blade_List;
+        OK           : constant Boolean := Grade (MV_B, K);
     begin
         if not OK then
             raise Inner_Product_Types_Exception with
               "Inner_Product_Types.Factorize_Blade Empty multivector detected.";
         else
             if K = 0 then
-                S := Scalar_Part (B);
+                S := Scalar_Part (MV_B);
             else
-                S := Norm_E (B);
+                S := Norm_E (MV_B);
             end if;
 
             if Scale'length > 0 then
@@ -176,32 +203,50 @@ package body Inner_Product_Types is
             end if;
 
             if K > 0 and S /= 0.0 then
-                E := Largest_Basis_Blade (B);
-                E_Array (0) := New_Basis_Blade (C3_Base'Enum_Val (K));
+                E := Largest_Basis_Blade (MV_B);
+                Lowest_Bit := Bits.Lowest_One_Bit (Bitmap (E));
+                Highest_Bit := Bits.Highest_One_Bit (Bitmap (E));
 
-                for Index_G in Unsigned_32 range 0 .. Unsigned_32 (Space_Dimension (B) - 1) loop
-                    Basis_Bit := Shift_Left (1, Integer (Index_G));
-                    if Bitmap (E) > 0 and Basis_Bit /= 0 then
-                        Idx := Idx + 1;
-                        E_Array (Idx) := New_Basis_Blade (C3_Base'Enum_Val (Basis_Bit), 1.0);
+                if K = 1 then
+                    Add_Multivector (Factors, Unit_E (MV_B));
+                else
+                    if Weight (E) < 0.0 then
+                        Scale (1) := - Scale (1);
+                        --  take care of orientation of blade:
+
+                        if (Unsigned_Integer (K) and 1) = 1 then
+                            Scale (1) := - Scale (1);
+                        end if;
                     end if;
-                end loop;
 
-                B_Current := Geometric_Product (B_Current, 1.0 / S);
-                for index in 0 .. K - 2 loop
-                    aFactor := New_Multivector (E_Array (index));
-                    aFactor := Inner_Product
-                      (Inner_Product (aFactor, B_Current, Left_Contraction),
-                       B_Current, Left_Contraction);
-                    aFactor := Unit_E (aFactor);
-                    Add_Multivector (Factors, aFactor);
-                    --  Remove aFactor from B_Current
-                    B_Current := Inner_Product (aFactor, B_Current, Left_Contraction);
-                end loop;
-                --  last factor = what is left of the input blade
-                --  B_Current is already normalized but
-                --  renormalize to remove any FP round-off error
-                Add_Multivector (Factors, Unit_E (B_Current));
+                    --  fix sign issues
+                    if (K mod 4) = 2 then
+                        Scale (1) := - Scale (1);
+                    end if;
+
+                    Blades_B := Blades (MV_B);
+
+                    for index in Lowest_Bit .. Highest_Bit loop
+                        Basis_Bit := Shift_Left (1, Integer (index));
+                        if Bitmap (E) > 0 and Basis_Bit /= 0 then
+                            Basis_Bitmap := Unsigned_32 (Bitmap (E)) xor Basis_Bit;
+                            for index_j in 1 .. List_Length (Blades_B) loop
+                                Blades_Bj := BB_Item (Blades_B, index_j);
+                                if (Unsigned_32 (Bitmap (Blades_Bj)) and Basis_Bitmap) =
+                                  Basis_Bitmap then
+                                    Vec_Bitmap := Unsigned_32 (Bitmap (Blades_Bj)) xor
+                                      Basis_Bitmap;
+                                    Sc := Weight (Blades_Bj) *
+                                      Canonical_Reordering_Sign
+                                        (Unsigned_Integer (Basis_Bitmap), Bitmap (Blades_Bj));
+                                    Blade.Add_Blade
+                                      (L_List, New_Basis_Blade (C3_Base'Enum_Val (Vec_Bitmap), Sc));
+                                end if;
+                            end loop;
+                        end if;
+                        Add_Multivector (Factors, New_Multivector (L_List));
+                    end loop;
+                end if;
             end if;
         end if;
         return  Factors;
