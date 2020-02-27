e3a7e408127382839d0c2e46a941ddff48bc8d8f
Roger Mc Murtrie <rogermc@grapevine.com.au>
2020-02-27 23:23:09 +1100

    Element 1 added to E2 and E3 element declarations

--- ga_ref_impl/src/blade_types.ads
+++ ga_ref_impl/src/blade_types.ads
@@ -2,8 +2,8 @@
 package Blade_Types is
 
    type BV_Base is (BV_e1e2, BV_e2e3, BV_e3e1);
-   type E2_Base is (E2_e1, E2_e2, E2_e1_e2);
-   type E3_Base is (E3_e1, E3_e2, E3_e3, E3_e1_e2, E3_e1_e3, E3_e2_e3,
+   type E2_Base is (E2_1, E2_e1, E2_e2, E2_e1_e2);
+   type E3_Base is (E3_1, E3_e1, E3_e2, E3_e3, E3_e1_e2, E3_e1_e3, E3_e2_e3,
                     E3_e1_e2_e3);
    type C3_Base is (C3_no, C3_e1, C3_e2, C3_e3, C3_ni,
                     C3_e1_e2, C3_e1_e3, C3_e2_e3,
@@ -15,9 +15,9 @@ package Blade_Types is
                     C3_e2_e3_no_ni, C3_e1_e2_e3_no_ni);
 private
    for BV_Base use (BV_e1e2 => 1, BV_e2e3 => 2, BV_e3e1 => 4);
-   for E2_Base use (E2_e1 => 1, E2_e2 => 2, E2_e1_e2 => 4);
+   for E2_Base use (E2_1 => 0, E2_e1 => 1, E2_e2 => 2, E2_e1_e2 => 4);
 
-   for E3_Base use (E3_e1 => 1, E3_e2 => 2, E3_e3 => 4,
+   for E3_Base use (E3_1 => 0, E3_e1 => 1, E3_e2 => 2, E3_e3 => 4,
                     E3_e1_e2    => 8, E3_e1_e3 => 16, E3_e2_e3 => 32,
                     E3_e1_e2_e3 => 64);
 
