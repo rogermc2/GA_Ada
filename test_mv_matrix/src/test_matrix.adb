8c5f422e122c791b0b4451a8e54c5bb948108dc1
rogermc2 <rogermc@iinet.net.au>
2020-03-19 10:35:20 +1100

    Updating test_matrix

--- test_mv_matrix/src/test_matrix.adb
+++ test_mv_matrix/src/test_matrix.adb
@@ -13,19 +13,25 @@ with C3GA;
 
 procedure Test_Matrix is
    use GA_Maths.Float_Array_Package;
---     no_bv   : Multivector := Basis_Vector (Blade_Types.C3_no);
---     e1_bv   : Multivector := Basis_Vector (Blade_Types.C3_e1);
---     e2_bv   : Multivector := Basis_Vector (Blade_Types.C3_e2);
---     e3_bv   : Multivector := Basis_Vector (Blade_Types.C3_e3);
---     ni_bv   : Multivector := Basis_Vector (Blade_Types.C3_ni);
+   no_bv   : Multivector := Basis_Vector (Blade_Types.C3_no);
+   e1_bv   : Multivector := Basis_Vector (Blade_Types.C3_e1);
+   e2_bv   : Multivector := Basis_Vector (Blade_Types.C3_e2);
+   e3_bv   : Multivector := Basis_Vector (Blade_Types.C3_e3);
+   ni_bv   : Multivector := Basis_Vector (Blade_Types.C3_ni);
 
    MV        : Multivectors.Normalized_Point :=
                  C3GA.Set_Normalized_Point (1.0, -0.5, 0.3);
+   Op23         : Multivector;
+   Op23_1       : Multivector;
    Dim       : constant Natural:= Space_Dimension (MV);
    Met       : Metric.Metric_Record (5) := Metric.C3_Metric;
    Matrix_AG : GA_Maths.Float_Matrix (0 .. 31, 0 .. 31);
    Mat_Inv   : GA_Maths.Float_Matrix (0 .. 31, 0 .. 31);
 begin
+   --  Multivector A = e1.add(e2.op(e3).op(e1));
+   --                = e1 + (e2^e3)^e1)
+   Op23 := Outer_Product (e2_bv, e3_bv);
+   Op23_1 := Outer_Product (Op23, e1_bv);
 --     Matrix_AG := Multivectors.To_Geometric_Matrix (MV, Met);
    Matrix_AG := Multivectors.Init_Geometric_Matrix (MV, Met);
    New_Line;
