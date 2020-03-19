d4bdd5e1711a2a7b72035d95ce11303a70c87c45
Roger Mc Murtrie <rogermc@grapevine.com.au>
2020-03-19 11:04:44 +1100

    Update

--- test_mv_matrix/src/test_matrix.adb
+++ test_mv_matrix/src/test_matrix.adb
@@ -21,18 +21,12 @@ procedure Test_Matrix is
 
    MV        : Multivectors.Normalized_Point :=
                  C3GA.Set_Normalized_Point (1.0, -0.5, 0.3);
-   Op23         : Multivector;
-   Op23_1       : Multivector;
    Dim       : constant Natural:= Space_Dimension (MV);
    Met       : Metric.Metric_Record (5) := Metric.C3_Metric;
    Matrix_AG : GA_Maths.Float_Matrix (0 .. 31, 0 .. 31);
    Mat_Inv   : GA_Maths.Float_Matrix (0 .. 31, 0 .. 31);
 begin
    --  Multivector A = e1.add(e2.op(e3).op(e1));
-   --                = e1 + (e2^e3)^e1)
-   Op23 := Outer_Product (e2_bv, e3_bv);
-   Op23_1 := Outer_Product (Op23, e1_bv);
---     Matrix_AG := Multivectors.To_Geometric_Matrix (MV, Met);
    Matrix_AG := Multivectors.Init_Geometric_Matrix (MV, Met);
    New_Line;
    GA_Utilities.Print_Matrix ("Test_Matrix Matrix_AG",
