48549dc13802056c5856ab42a28c93f12bd86833
rogermc2 <rogermc@iinet.net.au>
2020-05-01 15:24:08 +1000

    Condition_Number function coded

--- ga_lib/src/svd.adb
+++ ga_lib/src/svd.adb
@@ -4,23 +4,42 @@ with Ada.Text_IO; use Ada.Text_IO;
 
 with Ada_Lapack;
 
-with GA_Maths;
-
 package body SVD is
 
     package Complex_Maths   is new Ada.Numerics.Generic_Complex_Elementary_Functions (Complex_Types);
     package Lapack is new Ada_Lapack (Real, Complex_Types,
                                       Real_Arrays, Complex_Arrays);
 
+    --  ------------------------------------------------------------------------
+
+    function Singular_Value_Decomposition (aMatrix : GA_Maths.Float_Matrix)
+                                           return SVD is
+        use Complex_Arrays;
+        use Real_Arrays;
+        Converted_Matrix : Complex_Arrays.Complex_Matrix (aMatrix'Range (1), aMatrix'Range (2)) :=
+                             (others => (others => (0.0, 0.0)));
+    begin
+        for row in aMatrix'Range (1) loop
+            for col in aMatrix'Range (2) loop
+                Converted_Matrix (row, col) := (Real (aMatrix (row, col)), 0.0);
+            end loop;
+        end loop;
+        return Singular_Value_Decomposition (Converted_Matrix);
+    end Singular_Value_Decomposition;
+
+    --  ------------------------------------------------------------------------
+
     function Singular_Value_Decomposition (aMatrix : Real_Arrays.Real_Matrix)
                                            return SVD is
         use Complex_Arrays;
         Converted_Matrix : Complex_Arrays.Complex_Matrix (aMatrix'Range (1), aMatrix'Range (2)) :=
-        (others => (others => (0.0, 0.0)));
+                             (others => (others => (0.0, 0.0)));
     begin
         Set_Re (Converted_Matrix, aMatrix);
         return Singular_Value_Decomposition (Converted_Matrix);
     end Singular_Value_Decomposition;
+
+    --  ------------------------------------------------------------------------
 
     function Singular_Value_Decomposition (aMatrix : Complex_Arrays.Complex_Matrix)
                                            return SVD is
@@ -44,9 +63,9 @@ package body SVD is
         Vector_R        : Real_Vector (1 .. Num_Rows);
         Short_Vector    : Complex_Vector (1 .. 1);
         Num_Singular    : Integer := GA_Maths.Minimum (Num_Rows, Num_Cols);
-        Singular_Values : Real_Vector (1 .. Num_Singular);
+        Singular_Values : Real_Vector (1 .. Num_Singular);  --  sorted: S(i) >= S(i+1).
         Return_Code     : Integer := 0;
-        theSVD          : SVD (Num_Rows, Num_Cols);
+        theSVD          : SVD (Num_Rows, Num_Cols, Num_Singular);
         --  Return_Code  = 0:  successful exit.
         --            < 0:  if INFO = -i, the i-th argument had an illegal value.
         --            > 0:  if DBDSQR did not converge, INFO specifies how many
@@ -56,69 +75,61 @@ package body SVD is
         --  additional GESVD information
     begin
         GESVD (JOBU   => 'S', JOBVT  => 'S',
-                M      => Num_Rows, N      => Num_Cols,
-                A      => Matrix_A, LDA    => Num_Rows,
-                S      => Singular_Values,
-                U      => Matrix_U, LDU    => Num_Rows,
-                VT     => V_Transpose, LDVT   => Num_Cols,
-                WORK   => Short_Vector, LWORK  => -1,
-                RWORK  => Vector_R,
-                INFO   => Return_code);
+               M      => Num_Rows, N      => Num_Cols,
+               A      => Matrix_A, LDA    => Num_Rows,
+               S      => Singular_Values,
+               U      => Matrix_U, LDU    => Num_Rows,
+               VT     => V_Transpose, LDVT   => Num_Cols,
+               WORK   => Short_Vector, LWORK  => -1,
+               RWORK  => Vector_R,
+               INFO   => Return_code);
         declare
             Work_Vector_Rows : Integer := Integer (Re (Short_Vector (1)));
             Work_Vector      : Complex_Vector (1 .. Work_Vector_Rows);
         begin
             GESVD (JOBU   => 'S', JOBVT  => 'S',
-                M      => Num_Rows, N      => Num_Cols,
-                A      => Matrix_A, LDA    => Num_Rows,
-                S      => Singular_Values,
-                U      => Matrix_U, LDU    => Num_Rows,
-                VT     => V_Transpose, LDVT   => Num_Cols,
-                WORK   => Work_Vector, LWORK  => Work_Vector_Rows,
-                RWORK  => Vector_R,
-                INFO   => Return_Code);
+                   M      => Num_Rows, N      => Num_Cols,
+                   A      => Matrix_A, LDA    => Num_Rows,
+                   S      => Singular_Values,
+                   U      => Matrix_U, LDU    => Num_Rows,
+                   VT     => V_Transpose, LDVT   => Num_Cols,
+                   WORK   => Work_Vector, LWORK  => Work_Vector_Rows,
+                   RWORK  => Vector_R,
+                   INFO   => Return_Code);
+            if Return_Code = 0 then
+                theSVD.Matrix_W := Work_Vector;
+            end if;
         end;
+
         if Return_Code > 0 then
             raise SVD_Exception with
-             "Singular_Value_Decomposition failed with return code" & Integer'Image (Return_Code);
+              "Singular_Value_Decomposition failed with return code" & Integer'Image (Return_Code);
         else
             theSVD.Matrix_U := Matrix_U;
+            theSVD.Matrix_V := Transpose (V_Transpose);
+            theSVD.Sorted_Singular_Values := Singular_Values;
         end if;
         return theSVD;
     end Singular_Value_Decomposition;
 
     --  ------------------------------------------------------------------------
 
-    function Condition_Number (aMatrix : Real_Arrays.Real_Matrix) return Float is
---          use Float_Array_Package;
---          use Float_Functions;
---          use Float_List_Package;
-        --          Singular_Values : constant Float_List := Singular_Value_List (aMatrix);
-        --          Max             : Float;
-        --          Min             : Float;
-        Result          : Float := 0.0;
+    function Condition_Number (aMatrix : GA_Maths.Float_Matrix) return Float is
+        anSVD     : SVD := Singular_Value_Decomposition (aMatrix);
+        Max       : Float :=
+                      Float (anSVD.Sorted_Singular_Values (anSVD.Sorted_Singular_Values'First));
+        Min       : Float :=
+                      Float (anSVD.Sorted_Singular_Values (anSVD.Sorted_Singular_Values'Last));
+        Result    : Float := 0.0;
     begin
-        --          if List (Singular_Values) /= Empty_List then
-        --              Max := Singular_Values.First_Element;
-        --              Min := Singular_Values.Last_Element;
-        --              if Min > 0.0 then
-        --                  Result := Max / Min;
-        --              else
-        --                  Result := Max / 10.0 ** (-8);
-        --              end if;
-        --          end if;
+            if Min > 0.0 then
+                Result := Max / Min;
+            else
+                Result := Max / 10.0 ** (-8);
+            end if;
         return Result;
     end Condition_Number;
 
     --  ------------------------------------------------------------------------
-    --
-    --     function Sign (A, B : Float) return Float is
-    --          Result : Float := A;
-    --     begin
-    --          if B < 0.0 then
-    --              Result := -A;
-    --          end if;
-    --          return Result;
-    --     end Sign;
 
 end SVD;
