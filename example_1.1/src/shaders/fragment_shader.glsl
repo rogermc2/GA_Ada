41bfcd4d64f57a4604cd60c539881cc8c11a7615
Roger Mc Murtrie <rogermc@grapevine.com.au>
2020-05-07 11:43:48 +1000

    Update

--- example_1.1/src/shaders/fragment_shader.glsl
+++ example_1.1/src/shaders/fragment_shader.glsl
@@ -2,35 +2,36 @@
 
 //  Based on OGL_Turorials Tutorial 08_basic_shading
 
-in vec3 Position_Worldspace;
-in vec3 Camera_Normal;
-in vec3 Eye_Direction;
-in vec3 Light_Direction;
+//in vec3 Position_Worldspace;
+//in vec3 Camera_Normal;
+//in vec3 Eye_Direction;
+//in vec3 Light_Direction;
 
-out vec3 colour;
+out vec4 colour;
 
-uniform vec3 Light_Position_Worldspace;
-uniform vec4 Ambient_Colour;
-uniform vec4 Diffuse_Colour;
-uniform vec4 Drawing_Colour;
+//uniform vec3 Light_Position_Worldspace;
+//uniform vec4 Ambient_Colour;
+//uniform vec4 Diffuse_Colour;
+//uniform vec4 Drawing_Colour;
 
 void main()
     {
-    float LightPower = 50.0f;
-    // Distance to the light
-    float distance = length(Light_Position_Worldspace - Position_Worldspace);
-    
-    // Normal of the computed fragment in camera space
-    vec3 norm = normalize(Camera_Normal);
-    // Direction of the light (from the fragment to the light)
-    vec3 light_dir = normalize(Light_Direction);
-    // Cosine of the angle between the normal and the light direction,
-    // clamped above 0
-    //  - light is at the vertical of the triangle -> 1
-    //  - light is perpendicular to the triangle -> 0
-    //  - light is behind the triangle -> 0
-    float cosTheta = clamp(dot(norm, light_dir), 0, 1);
-    
-    vec3 LightColour = (Drawing_Colour + Ambient_Colour + Diffuse_Colour * LightPower * cosTheta / (distance * distance)).xyz;
-    colour = LightColour;
+//    float LightPower = 50.0f;
+//    // Distance to the light
+//    float distance = length(Light_Position_Worldspace - Position_Worldspace);
+//
+//    // Normal of the computed fragment in camera space
+//    vec3 norm = normalize(Camera_Normal);
+//    // Direction of the light (from the fragment to the light)
+//    vec3 light_dir = normalize(Light_Direction);
+//    // Cosine of the angle between the normal and the light direction,
+//    // clamped above 0
+//    //  - light is at the vertical of the triangle -> 1
+//    //  - light is perpendicular to the triangle -> 0
+//    //  - light is behind the triangle -> 0
+//    float cosTheta = clamp(dot(norm, light_dir), 0, 1);
+//
+//    vec3 LightColour = (Drawing_Colour + Ambient_Colour + Diffuse_Colour * LightPower * cosTheta / (distance * distance)).xyz;
+    //  colour = LightColour;
+    colour = vec4(1.0, 0.0, 0.0, 1.0);
     }
