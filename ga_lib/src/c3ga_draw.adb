1ed62e1ef139a30a20b2cdda9247913701b84014
rogermc2 <rogermc@iinet.net.au>
2020-07-04 15:12:05 +1000

    Remove debug statements

--- ga_lib/src/c3ga_draw.adb
+++ ga_lib/src/c3ga_draw.adb
@@ -1,7 +1,7 @@
 
 with Ada.Text_IO; use Ada.Text_IO;
 
-with Utilities;
+--  with Utilities;
 
 with C3GA;
 with GA_Draw;
@@ -131,13 +131,10 @@ package body C3GA_Draw is
                        C3GA.To_VectorE3GA (Analysis.M_Vectors (1));
         Scale      : Float := Analysis.Scalars (1);
     begin
---          Utilities.Print_Vector ("C3GA_Draw.Draw_Flat Analysis.Points", Analysis.Points (1));
         case Analysis.M_Type.Blade_Subclass is
             when Line_Subclass =>
                 Put_Line ("C3GA_Draw.Draw_Flat Line.");
                 --  Draw_Line doesn't use method
-                Utilities.Print_Vector ("C3GA_Draw.Draw_Flat Point_Pos", Point_Pos);
-                Utilities.Print_Vector ("C3GA_Draw.Draw_Flat Direction", Direction);
                 GA_Draw.Draw_Line (Render_Program, Model_View_Matrix,
                                    Point_Pos, Direction, Scale);
             when Plane_Subclass =>
