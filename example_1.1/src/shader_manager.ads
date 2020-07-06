bf71da1aff7da59da7d165007433fd446df81e9a
rogermc2 <rogermc@iinet.net.au>
2020-07-06 14:30:30 +1000

    Added Line width

--- example_1.1/src/shader_manager.ads
+++ example_1.1/src/shader_manager.ads
@@ -14,6 +14,7 @@ package Shader_Manager is
         Diffuse_Colour_ID    : GL.Uniforms.Uniform := 0;
         Drawing_Colour_ID    : GL.Uniforms.Uniform := 0;
         Light_Position_ID    : GL.Uniforms.Uniform := 0;
+        Line_Width_ID        : GL.Uniforms.Uniform := 0;
         Model_Matrix_ID      : GL.Uniforms.Uniform := 0;
         Model_View_Matrix_ID : GL.Uniforms.Uniform := 0;
         Projection_Matrix_ID : GL.Uniforms.Uniform := 0;
@@ -25,6 +26,7 @@ package Shader_Manager is
     procedure Set_Diffuse_Colour (Diffuse_Colour : Singles.Vector4);
     procedure Set_Drawing_Colour (Drawing_Colour : Singles.Vector4);
     procedure Set_Light_Position_Vector (Light_Position : Singles.Vector3);
+    procedure Set_Line_Width (Width : Single);
     procedure Set_Model_Matrix (Model_Matrix : Singles.Matrix4);
     procedure Set_Model_View_Matrix (Model_View_Matrix : Singles.Matrix4);
     procedure Set_Projection_Matrix (Projection_Matrix : Singles.Matrix4);
