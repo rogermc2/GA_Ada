e912241fccbc2e4fe62d47280cac779c4ff30373
rogermc2 <rogermc@iinet.net.au>
2020-07-19 11:37:16 +1000

    Moved BV_Names generation to package initialization.

--- ga_ref_impl/src/blade_types.adb
+++ ga_ref_impl/src/blade_types.adb
@@ -1,17 +1,21 @@
 
 package body Blade_Types is
 
+    BV_Names : Basis_Vector_Names;
+
+    --  -------------------------------------------------------------------------
+
     function Basis_Names_C3GA return Basis_Vector_Names is
-        BV_Names : Basis_Vector_Names;
     begin
-        BV_Names.Append (To_Unbounded_String ("no"));
-        BV_Names.Append (To_Unbounded_String ("e1"));
-        BV_Names.Append (To_Unbounded_String ("e2"));
-        BV_Names.Append (To_Unbounded_String ("e3"));
-        BV_Names.Append (To_Unbounded_String ("ni"));
         return BV_Names;
     end Basis_Names_C3GA;
 
     --  -------------------------------------------------------------------------
 
+begin
+    BV_Names.Append (To_Unbounded_String ("no"));
+    BV_Names.Append (To_Unbounded_String ("e1"));
+    BV_Names.Append (To_Unbounded_String ("e2"));
+    BV_Names.Append (To_Unbounded_String ("e3"));
+    BV_Names.Append (To_Unbounded_String ("ni"));
 end Blade_Types;
