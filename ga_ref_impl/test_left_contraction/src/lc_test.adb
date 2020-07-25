57c96dfdb437e35e8c1658ff173b1d093042b5da
Roger Mc Murtrie <rogermc@grapevine.com.au>
2020-07-25 22:23:44 +1000

    Improved printout

--- ga_ref_impl/test_left_contraction/src/lc_test.adb
+++ ga_ref_impl/test_left_contraction/src/lc_test.adb
@@ -14,13 +14,6 @@ with Multivector_Type;
 procedure LC_Test is
    use Blade_Types;
    BV_Names      : Basis_Vector_Names;
---     C3_Met_Matrix : constant Metric.Metric_Matrix (1 .. 5, 1 .. 5) :=
---                      ((0.0, 0.0, 0.0, 0.0, -1.0),
---                       (0.0, 1.0, 0.0, 0.0, 0.0),
---                       (0.0, 0.0, 1.0, 0.0, 0.0),
---                       (0.0, 0.0, 0.0 ,1.0, 0.0),
---                       (-1.0, 0.0, 0.0 , 0.0, 0.0));
---     C3_Metric      : constant Metric.Metric_Record := Metric.New_Metric (C3_Met_Matrix);
 
    no_bv        : Multivector := Basis_Vector (Blade_Types.C3_no);
    e1_bv        : Multivector := Basis_Vector (Blade_Types.C3_e1);
@@ -28,12 +21,15 @@ procedure LC_Test is
    e3_bv        : Multivector := Basis_Vector (Blade_Types.C3_e3);
    ni_bv        : Multivector := Basis_Vector (Blade_Types.C3_ni);
 
+   Met          : Metric_Record := C3_Metric;
+
    MV_A1        : Multivector;
    MV_A2        : Multivector;
    MV_A         : Multivector;
    MV_A_Inv    : Multivector;
 
-   NP_1         : constant Multivector := New_Normalized_Point (-0.356756, -0.881980, 0.0);
+   NP_1         : constant Multivector :=
+                    New_Normalized_Point (-0.356756, -0.881980, 0.0);
    NP_1_OP      : Multivector;
    NP_1_IP      : Multivector;
    NP_1_GP_Met  : Multivector;
@@ -42,33 +38,34 @@ procedure LC_Test is
    NP_1_GP      : Multivector;
 
 begin
+   Multivectors.Set_Geometry (C3_Geometry);
    BV_Names.Append (Ada.Strings.Unbounded.To_Unbounded_String ("no"));
    BV_Names.Append (Ada.Strings.Unbounded.To_Unbounded_String ("e1"));
    BV_Names.Append (Ada.Strings.Unbounded.To_Unbounded_String ("e2"));
    BV_Names.Append (Ada.Strings.Unbounded.To_Unbounded_String ("e3"));
    BV_Names.Append (Ada.Strings.Unbounded.To_Unbounded_String ("ni"));
 
-   MV_A1 := Left_Contraction (e2_bv, e3_bv);
-   MV_A2 := Left_Contraction (e2_bv, ni_bv);
-   MV_A := e1_bv + Outer_Product (Outer_Product (e2_bv, e3_bv), e1_bv);
-   GA_Utilities.Print_Multivector_String ("LC_Test MV_A1", MV_A1, Basis_Names_C3GA);
-   GA_Utilities.Print_Multivector_String ("LC_Test MV_A2", MV_A2, Basis_Names_C3GA);
-   GA_Utilities.Print_Multivector_String ("LC_Test MV_A", MV_A, Basis_Names_C3GA);
+   MV_A1 := Left_Contraction (e2_bv, e3_bv, Met);
+   MV_A2 := Left_Contraction (e2_bv, ni_bv, Met);
+--     MV_A := e1_bv + Outer_Product (Outer_Product (e2_bv, e3_bv), e1_bv);
+   GA_Utilities.Print_Multivector_String ("LC_Test LC (e2, e3)", MV_A1, Basis_Names_C3GA);
+   GA_Utilities.Print_Multivector_String ("LC_Test LC (e2, ni)", MV_A2, Basis_Names_C3GA);
+--     GA_Utilities.Print_Multivector_String ("LC_Test MV_A", MV_A, Basis_Names_C3GA);
 
-   MV_A1 := Left_Contraction (e3_bv, e2_bv);
-   MV_A2 := Left_Contraction (ni_bv, e2_bv);
-   GA_Utilities.Print_Multivector_String ("LC_Test MV_A1", MV_A1, Basis_Names_C3GA);
-   GA_Utilities.Print_Multivector_String ("LC_Test MV_A2", MV_A2, Basis_Names_C3GA);
+   MV_A1 := Left_Contraction (e3_bv, e2_bv, Met);
+   MV_A2 := Left_Contraction (ni_bv, e2_bv, Met);
+   GA_Utilities.Print_Multivector_String ("LC_Test LC (e3, e2)", MV_A1, Basis_Names_C3GA);
+   GA_Utilities.Print_Multivector_String ("LC_Test LC (n1, e2)", MV_A2, Basis_Names_C3GA);
 
-   MV_A1 := Left_Contraction (e3_bv, Geometric_Product (e2_bv, e3_bv));
-   MV_A2 := Left_Contraction (ni_bv, Geometric_Product (e2_bv, ni_bv));
-   GA_Utilities.Print_Multivector_String ("LC_Test MV_A1", MV_A1, Basis_Names_C3GA);
-   GA_Utilities.Print_Multivector_String ("LC_Test MV_A2", MV_A2, Basis_Names_C3GA);
+   MV_A1 := Left_Contraction (e3_bv, Geometric_Product (e2_bv, e3_bv, Met), Met);
+   MV_A2 := Left_Contraction (ni_bv, Geometric_Product (e2_bv, ni_bv, Met), Met);
+   GA_Utilities.Print_Multivector_String ("LC_Test LC (e3, GP (e2, e3))", MV_A1, Basis_Names_C3GA);
+   GA_Utilities.Print_Multivector_String ("LC_Test (ni, GP (e2, ni))", MV_A2, Basis_Names_C3GA);
 
-   MV_A1 := Left_Contraction (e3_bv, Geometric_Product (e3_bv, e2_bv));
-   MV_A2 := Left_Contraction (ni_bv, Geometric_Product (ni_bv, e2_bv));
-   GA_Utilities.Print_Multivector_String ("LC_Test MV_A1", MV_A1, Basis_Names_C3GA);
-   GA_Utilities.Print_Multivector_String ("LC_Test MV_A2", MV_A2, Basis_Names_C3GA);
+   MV_A1 := Left_Contraction (e3_bv, Geometric_Product (e3_bv, e2_bv, Met), Met);
+   MV_A2 := Left_Contraction (ni_bv, Geometric_Product (ni_bv, e2_bv, Met), Met);
+   GA_Utilities.Print_Multivector_String ("LC_Test (e3, GP (e3, e2))", MV_A1, Basis_Names_C3GA);
+   GA_Utilities.Print_Multivector_String ("LC_Test (ni, GP (ni, e2))", MV_A2, Basis_Names_C3GA);
 --     MV_A_Inv := General_Inverse (MV_A);
 --     GA_Utilities.Print_Multivector ("LC_Test MV_A_Inv", MV_A_Inv);
 --
