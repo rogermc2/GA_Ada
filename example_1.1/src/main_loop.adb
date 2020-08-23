9e6f17a596d1be27508ba9184777233b8d4b7d31
Roger Mc Murtrie <rogermc@grapevine.com.au>
2020-08-23 23:44:11 +1000

    Moved plane forward

--- example_1.1/src/main_loop.adb
+++ example_1.1/src/main_loop.adb
@@ -144,7 +144,7 @@ procedure Main_Loop (Main_Window : in out Glfw.Windows.Window) is
          Put_Line ("Main_Loop.Display drawing rotated circle.");
          --  draw rotated circle
          Shader_Manager.Set_Ambient_Colour (Green);
-         Translation_Matrix := Maths.Translation_Matrix ((0.0, 2.0, 0.0)) *
+         Translation_Matrix := Maths.Translation_Matrix ((0.0, 2.0, 10.0)) *
            Translation_Matrix;
          Shader_Manager.Set_Translation_Matrix (Translation_Matrix);
          Palet.Set_Draw_Mode_On (Palet.OD_Orientation);
@@ -176,7 +176,7 @@ procedure Main_Loop (Main_Window : in out Glfw.Windows.Window) is
             Alpha := Alpha + 0.1;
          end loop;
 
-         Translation_Matrix := Maths.Translation_Matrix ((0.0, -2.0, 0.0)) *
+         Translation_Matrix := Maths.Translation_Matrix ((0.0, -2.0, 10.0)) *
            Translation_Matrix;
          Shader_Manager.Set_Translation_Matrix (Translation_Matrix);
 
@@ -185,6 +185,9 @@ procedure Main_Loop (Main_Window : in out Glfw.Windows.Window) is
          Put_Line ("Main_Loop.Display drawing plane.");
          Shader_Manager.Set_Ambient_Colour (Yellow);
          Draw_1_1.Draw_Plane (Render_Graphic_Program, aDual_Plane);
+         Translation_Matrix := Maths.Translation_Matrix ((0.0, 0.0, -10.0)) *
+           Translation_Matrix;
+         Shader_Manager.Set_Translation_Matrix (Translation_Matrix);
       end if;
 
    exception
