c5fe773391b6c62d8b8d5e30b467f10f3ebd1406
Roger Mc Murtrie <rogermc@grapevine.com.au>
2020-08-24 16:14:18 +1000

    Pick coding

--- ga_lib/src/gl_util.adb
+++ ga_lib/src/gl_util.adb
@@ -14,212 +14,276 @@ with E3GA_Utilities;
 
 package body GL_Util is
 
-    --  ------------------------------------------------------------------
-    --  Load_Pick_Matrix
-    procedure Load_Pick_Matrix is
-    begin
-        null;
-    end Load_Pick_Matrix;
+   Pick_Is_Active      : Boolean := False;  --  set to true during picking
+   --  set to picking window (x, y, w, h) during picking
+   Pick_Window_Open    : Pick_Window := (others => 0.0);
+   --  Must be set correctly by caller of pick() to get correct distances returned
+   Frustum_Near_Pos    : Single := 1.0;
+   --  Must be set correctly by caller of pick() to get correct distances returned
+   Frustum_Far_Pos     : Single := 100.0;
+   --  not required for pick(), provided for completenes
+   Width_Of_Frustum    : Single := 0.0;
+   --  not required for pick(), provided for completenes
+   Height_Of_Frustum   : Single := 0.0;
+   Size_Of_Pick_Window : Int := 4;
 
-    --  ------------------------------------------------------------------
+   --  ------------------------------------------------------------------
 
-    procedure Print_GL_Int3_Array
-      (Name : String; anArray : GL.Types.Ints.Vector3_Array) is
-    begin
-        Put_Line (Name & ": ");
-        for Index in anArray'First .. anArray'Last loop
-            Utilities.Print_Vector ("", anArray (Index));
-        end loop;
-        New_Line;
-    end Print_GL_Int3_Array;
+   function Frustum_Near return Single is
+   begin
+      return Frustum_Near_Pos;
+   end Frustum_Near;
 
-    --  ------------------------------------------------------------------------
+   --  ------------------------------------------------------------------
 
-    --  Rotor_GL_Multiply multiplies GL_Matrix by rotor 'R'
-    procedure Rotor_GL_Multiply (R : Multivectors.Rotor;
-                                 GL_Matrix : in out GL.Types.Singles.Matrix4) is
-        use E3GA;
-        use Multivectors;
-        use GL;
-        use GL.Types.Singles;
-        IR        : constant Rotor := To_Rotor (General_Inverse (R));
-        E_Rot     : Multivectors.Multivector;
-        E1_IR     : Multivectors.Multivector;
-        Image     : Vector3_Array (1 .. 4);
-        VC        : Vector3;
-        Matrix    : Matrix4 := Identity4;
-        Image_Row : Int := 0;
-    begin
-        E1_IR := Geometric_Product (e1, IR);
-        if not Is_Null (E1_IR ) then
-            --  compute the images of all OpenGL basis vectors
-            E_Rot := Geometric_Product (R, E1_IR);
-            Image (1) := To_GL (E_Rot);
-            E_Rot := Geometric_Product (R, Geometric_Product (e2, IR));
-            Image (2) := To_GL (E_Rot);
-            E_Rot := Geometric_Product (R, Geometric_Product (e3, IR));
-            Image (3) := To_GL (E_Rot);
-            Image (4) := (0.0, 0.0, 0.0);  -- Image of origin
-            --  Transfer the coordinates to the OpenGL matrix
-            for row in GL.Index_Homogeneous loop
-                Image_Row := Image_Row + 1;
-                VC := Image (Image_Row);
-                Matrix (row, X) := VC (X);
-                Matrix (row, Y) := VC (Y);
-                Matrix (row, Z) := VC (Z);
-            end loop;
-            GL_Matrix := Matrix * GL_Matrix;
-        end if;
+   function Frustum_Far return Single is
+   begin
+      return Frustum_Far_Pos;
+   end Frustum_Far;
 
-    exception
-        when others =>
-            Put_Line ("An exception occurred in GL_Util.Rotor_GL_Multiply.");
-            raise;
-    end Rotor_GL_Multiply;
+   --  ------------------------------------------------------------------
 
-    --  -------------------------------------------------------------------------
-    --  Pick_Matrix defines a picking region
-    procedure Pick_Matrix (Centre_X, Centre_Y : GL.Types.Size;
-                           Width, Height : GL.Types.Size) is
-    begin
-        GL.Window.Set_Viewport (Centre_X, Centre_Y, Width, Height);
-    end Pick_Matrix;
+   function Frustum_Width return Single is
+   begin
+      return Width_Of_Frustum;
+   end Frustum_Width;
 
-    --  ------------------------------------------------------------------
+   --  ------------------------------------------------------------------
 
-    function Rotor_To_GL_Matrix (R : Multivectors.Rotor)
-                                 return  GL.Types.Singles.Matrix4 is
-        use GL;
-        M3        : GA_Maths.GA_Matrix3;
-        GL_Matrix : GL.Types.Singles.Matrix4 := GL.Types.Singles.Identity4;
-        Mrow      : integer := 0;
-        Mcol      : integer := 0;
-    begin
-        E3GA_Utilities.Rotor_To_Matrix (R, M3);
-        for row in Index_Homogeneous range X .. Z loop
-            Mrow := Mrow + 1;
-            for col in Index_Homogeneous range X .. Z loop
-                Mcol := Mcol + 1;
-                GL_Matrix (col, row) := GL.Types.Single (M3 (Mrow, Mcol));
-            end loop;
-            Mcol := 0;
-        end loop;
-        return GL_Matrix;
+   function Frustum_Height return Single is
+   begin
+      return Height_Of_Frustum;
+   end Frustum_Height;
 
-    exception
-        when others =>
-            Put_Line ("An exception occurred in GL_Util.Rotor_To_GL_Matrix.");
-            raise;
-    end Rotor_To_GL_Matrix;
+   --  ------------------------------------------------------------------
 
-    --  -------------------------------------------------------------------------
+   function OpenGL_Pick return Pick_Window is
+   begin
+      return Pick_Window_Open;
+   end OpenGL_Pick;
 
-    function To_GL (V3 : Multivectors.Multivector) return GL.Types.Doubles.Vector3 is
-        use Interfaces;
-        use Blade.Blade_List_Package;
-        use Blade;
-        use Blade_Types;
-        Blades  : constant Blade.Blade_List := Multivectors.Get_Blade_List (V3);
-        Curs    : Cursor := Blades.First;
-        BM      : Unsigned_32;
-        Value   : Double;
-        Val_X   : Double := 0.0;
-        Val_Y   : Double := 0.0;
-        Val_Z   : Double := 0.0;
-    begin
-        while Has_Element (Curs) loop
-            BM := Bitmap (Element (Curs));
-            Value := Double (Blade.Weight (Element (Curs)));
-            if (BM and E3_Base'Enum_Rep (E3_e1)) /= 0 then
-                Val_X := Val_X + Value;
-            end if;
-            if (BM and E3_Base'Enum_Rep (E3_e2)) /= 0 then
-                Val_Y := Val_Y + Value;
-            end if;
-            if (BM and E3_Base'Enum_Rep (E3_e3)) /= 0 then
-                Val_Z := Val_Z + Value;
-            end if;
-            Next (Curs);
-        end loop;
-        return (Val_X, Val_Y, Val_Z);
+   --  ------------------------------------------------------------------
 
-    exception
-        when others =>
-            Put_Line ("An exception occurred in GL_Util.To_GL Double.");
-            raise;
-    end To_GL;
+   procedure Load_Pick_Matrix is
+   begin
+      if Pick_Is_Active then
+         null;
+      end if;
+   end Load_Pick_Matrix;
 
-    --  -------------------------------------------------------------------------
+   --  ------------------------------------------------------------------
 
-    function To_GL (V3 : Multivectors.Multivector) return GL.Types.Singles.Vector3 is
-        use Interfaces;
-        use Blade.Blade_List_Package;
-        use Blade;
-        use Blade_Types;
-        Blades  : constant Blade.Blade_List := Multivectors.Get_Blade_List (V3);
-        Curs    : Cursor := Blades.First;
-        BM      : Unsigned_32;
-        Value   : Single;
-        Val_X   : Single := 0.0;
-        Val_Y   : Single := 0.0;
-        Val_Z   : Single := 0.0;
-    begin
-        while Has_Element (Curs) loop
-            BM := Bitmap (Element (Curs));
-            Value := Single (Blade.Weight (Element (Curs)));
-            if (BM and E3_Base'Enum_Rep (E3_e1)) /= 0 then
-                Val_X := Val_X + Value;
-            end if;
-            if (BM and E3_Base'Enum_Rep (E3_e2)) /= 0 then
-                Val_Y := Val_Y + Value;
-            end if;
-            if (BM and E3_Base'Enum_Rep (E3_e3)) /= 0 then
-                Val_Z := Val_Z + Value;
-            end if;
-            Next (Curs);
-        end loop;
-        return (Val_X, Val_Y, Val_Z);
+   function Pick_Active return Boolean is
+   begin
+      return Pick_Is_Active;
+   end Pick_Active;
 
-    exception
-        when others =>
-            Put_Line ("An exception occurred in GL_Util.To_GL Single.");
-            raise;
-    end To_GL;
+   --  ------------------------------------------------------------------
 
-    --  -------------------------------------------------------------------------
+   function Pick_Window_Size return Int is
+   begin
+      return Size_Of_Pick_Window;
+   end Pick_Window_Size;
 
-    function To_GL (V3 : GA_Maths.Float_3D) return GL.Types.Singles.Vector3 is
-    begin
-        return (Single (V3 (1)), Single (V3 (2)), Single (V3 (3)));
-    end To_GL;
+   --  ------------------------------------------------------------------
 
-    --  ---------------------------------------------------------------------
+   procedure Print_GL_Int3_Array
+     (Name : String; anArray : GL.Types.Ints.Vector3_Array) is
+   begin
+      Put_Line (Name & ": ");
+      for Index in anArray'First .. anArray'Last loop
+         Utilities.Print_Vector ("", anArray (Index));
+      end loop;
+      New_Line;
+   end Print_GL_Int3_Array;
 
-    procedure Viewport_Coordinates (Pt_World : GA_Maths.Float_3D;
-                                    Model_View_Matrix,
-                                    Projection_Matrix : GL.Types.Singles.Matrix4;
-                                    Coords : out GL.Types.Singles.Vector2) is
-        use GL;
-        use GL.Types.Singles;
-        VP_X          : Int;
-        VP_Y          : Int;
-        Window_Width  : Size;
-        Window_Height : Size;
-        PT1           : constant Vector4 := (Single (Pt_World (1)), Single (Pt_World (2)),
-                                             Single (Pt_World (3)), 1.0);
-        PT2           : constant Vector4 := Projection_Matrix * Model_View_Matrix * PT1;
-    begin
-        --   PT1 := Projection_Matrix * PT2;
-        GL.Window.Get_Viewport (VP_X, VP_Y, Window_Width, Window_Height);
-        Coords (X) := Single (VP_X) + (1.0 + PT2 (X) / PT2 (W)) * Single (Window_Width) / 2.0;
-        Coords (Y) := Single (VP_Y) + (1.0 + PT2 (Y) / PT2 (W)) * Single (Window_Height) / 2.0;
+   --  ------------------------------------------------------------------------
 
-    exception
-        when others =>
-            Put_Line ("An exception occurred in GL_Util.Viewport_Coordinates.");
-            raise;
-    end Viewport_Coordinates;
+   --  Rotor_GL_Multiply multiplies GL_Matrix by rotor 'R'
+   procedure Rotor_GL_Multiply (R         : Multivectors.Rotor;
+                                GL_Matrix : in out GL.Types.Singles.Matrix4) is
+      use E3GA;
+      use Multivectors;
+      use GL;
+      use GL.Types.Singles;
+      IR        : constant Rotor := To_Rotor (General_Inverse (R));
+      E_Rot     : Multivectors.Multivector;
+      E1_IR     : Multivectors.Multivector;
+      Image     : Vector3_Array (1 .. 4);
+      VC        : Vector3;
+      Matrix    : Matrix4 := Identity4;
+      Image_Row : Int := 0;
+   begin
+      E1_IR := Geometric_Product (e1, IR);
+      if not Is_Null (E1_IR ) then
+         --  compute the images of all OpenGL basis vectors
+         E_Rot := Geometric_Product (R, E1_IR);
+         Image (1) := To_GL (E_Rot);
+         E_Rot := Geometric_Product (R, Geometric_Product (e2, IR));
+         Image (2) := To_GL (E_Rot);
+         E_Rot := Geometric_Product (R, Geometric_Product (e3, IR));
+         Image (3) := To_GL (E_Rot);
+         Image (4) := (0.0, 0.0, 0.0);  -- Image of origin
+         --  Transfer the coordinates to the OpenGL matrix
+         for row in GL.Index_Homogeneous loop
+            Image_Row := Image_Row + 1;
+            VC := Image (Image_Row);
+            Matrix (row, X) := VC (X);
+            Matrix (row, Y) := VC (Y);
+            Matrix (row, Z) := VC (Z);
+         end loop;
+         GL_Matrix := Matrix * GL_Matrix;
+      end if;
 
-    --  -------------------------------------------------------------------------
+   exception
+      when others =>
+         Put_Line ("An exception occurred in GL_Util.Rotor_GL_Multiply.");
+         raise;
+   end Rotor_GL_Multiply;
+
+   --  -------------------------------------------------------------------------
+   --  Pick_Matrix defines a picking region
+   procedure Pick_Matrix (Centre_X, Centre_Y : GL.Types.Size;
+                          Width, Height      : GL.Types.Size) is
+   begin
+      GL.Window.Set_Viewport (Centre_X, Centre_Y, Width, Height);
+   end Pick_Matrix;
+
+   --  ------------------------------------------------------------------
+
+   function Rotor_To_GL_Matrix (R : Multivectors.Rotor)
+                                return  GL.Types.Singles.Matrix4 is
+      use GL;
+      M3        : GA_Maths.GA_Matrix3;
+      GL_Matrix : GL.Types.Singles.Matrix4 := GL.Types.Singles.Identity4;
+      Mrow      : integer := 0;
+      Mcol      : integer := 0;
+   begin
+      E3GA_Utilities.Rotor_To_Matrix (R, M3);
+      for row in Index_Homogeneous range X .. Z loop
+         Mrow := Mrow + 1;
+         for col in Index_Homogeneous range X .. Z loop
+            Mcol := Mcol + 1;
+            GL_Matrix (col, row) := GL.Types.Single (M3 (Mrow, Mcol));
+         end loop;
+         Mcol := 0;
+      end loop;
+      return GL_Matrix;
+
+   exception
+      when others =>
+         Put_Line ("An exception occurred in GL_Util.Rotor_To_GL_Matrix.");
+         raise;
+   end Rotor_To_GL_Matrix;
+
+   --  -------------------------------------------------------------------------
+
+   function To_GL (V3 : Multivectors.Multivector) return GL.Types.Doubles.Vector3 is
+      use Interfaces;
+      use Blade.Blade_List_Package;
+      use Blade;
+      use Blade_Types;
+      Blades  : constant Blade.Blade_List := Multivectors.Get_Blade_List (V3);
+      Curs    : Cursor := Blades.First;
+      BM      : Unsigned_32;
+      Value   : Double;
+      Val_X   : Double := 0.0;
+      Val_Y   : Double := 0.0;
+      Val_Z   : Double := 0.0;
+   begin
+      while Has_Element (Curs) loop
+         BM := Bitmap (Element (Curs));
+         Value := Double (Blade.Weight (Element (Curs)));
+         if (BM and E3_Base'Enum_Rep (E3_e1)) /= 0 then
+            Val_X := Val_X + Value;
+         end if;
+         if (BM and E3_Base'Enum_Rep (E3_e2)) /= 0 then
+            Val_Y := Val_Y + Value;
+         end if;
+         if (BM and E3_Base'Enum_Rep (E3_e3)) /= 0 then
+            Val_Z := Val_Z + Value;
+         end if;
+         Next (Curs);
+      end loop;
+      return (Val_X, Val_Y, Val_Z);
+
+   exception
+      when others =>
+         Put_Line ("An exception occurred in GL_Util.To_GL Double.");
+         raise;
+   end To_GL;
+
+   --  -------------------------------------------------------------------------
+
+   function To_GL (V3 : Multivectors.Multivector) return GL.Types.Singles.Vector3 is
+      use Interfaces;
+      use Blade.Blade_List_Package;
+      use Blade;
+      use Blade_Types;
+      Blades  : constant Blade.Blade_List := Multivectors.Get_Blade_List (V3);
+      Curs    : Cursor := Blades.First;
+      BM      : Unsigned_32;
+      Value   : Single;
+      Val_X   : Single := 0.0;
+      Val_Y   : Single := 0.0;
+      Val_Z   : Single := 0.0;
+   begin
+      while Has_Element (Curs) loop
+         BM := Bitmap (Element (Curs));
+         Value := Single (Blade.Weight (Element (Curs)));
+         if (BM and E3_Base'Enum_Rep (E3_e1)) /= 0 then
+            Val_X := Val_X + Value;
+         end if;
+         if (BM and E3_Base'Enum_Rep (E3_e2)) /= 0 then
+            Val_Y := Val_Y + Value;
+         end if;
+         if (BM and E3_Base'Enum_Rep (E3_e3)) /= 0 then
+            Val_Z := Val_Z + Value;
+         end if;
+         Next (Curs);
+      end loop;
+      return (Val_X, Val_Y, Val_Z);
+
+   exception
+      when others =>
+         Put_Line ("An exception occurred in GL_Util.To_GL Single.");
+         raise;
+   end To_GL;
+
+   --  -------------------------------------------------------------------------
+
+   function To_GL (V3 : GA_Maths.Float_3D) return GL.Types.Singles.Vector3 is
+   begin
+      return (Single (V3 (1)), Single (V3 (2)), Single (V3 (3)));
+   end To_GL;
+
+   --  ---------------------------------------------------------------------
+
+   procedure Viewport_Coordinates (Pt_World          : GA_Maths.Float_3D;
+                                   Model_View_Matrix,
+                                   Projection_Matrix : GL.Types.Singles.Matrix4;
+                                   Coords            : out GL.Types.Singles.Vector2) is
+      use GL;
+      use GL.Types.Singles;
+      VP_X          : Int;
+      VP_Y          : Int;
+      Window_Width  : Size;
+      Window_Height : Size;
+      PT1           : constant Vector4 := (Single (Pt_World (1)), Single (Pt_World (2)),
+                                           Single (Pt_World (3)), 1.0);
+      PT2           : constant Vector4 := Projection_Matrix * Model_View_Matrix * PT1;
+   begin
+      --   PT1 := Projection_Matrix * PT2;
+      GL.Window.Get_Viewport (VP_X, VP_Y, Window_Width, Window_Height);
+      Coords (X) := Single (VP_X) + (1.0 + PT2 (X) / PT2 (W)) * Single (Window_Width) / 2.0;
+      Coords (Y) := Single (VP_Y) + (1.0 + PT2 (Y) / PT2 (W)) * Single (Window_Height) / 2.0;
+
+   exception
+      when others =>
+         Put_Line ("An exception occurred in GL_Util.Viewport_Coordinates.");
+         raise;
+   end Viewport_Coordinates;
+
+   --  -------------------------------------------------------------------------
 
 end GL_Util;
--- ga_lib/src/gl_util.ads
+++ ga_lib/src/gl_util.ads
@@ -5,40 +5,30 @@ with GA_Maths;
 with Multivectors;
 
 package GL_Util is
-    subtype Pick_Window is Singles.Vector4;
 
-    type GL_Pick is private;
+   subtype Pick_Window is Singles.Vector4;
 
-    procedure Load_Pick_Matrix;
-    procedure Pick_Matrix (Centre_X, Centre_Y : GL.Types.Size;
-                           Width, Height : GL.Types.Size);
-    procedure Print_GL_Int3_Array
-      (Name : String; anArray : GL.Types.Ints.Vector3_Array);
-    function Rotor_To_GL_Matrix (R : Multivectors.Rotor)
+   function Frustum_Near return Single;
+   function Frustum_Far return Single;
+   function Frustum_Width return Single;
+   function Frustum_Height return Single;
+   procedure Load_Pick_Matrix;
+   function OpenGL_Pick return Pick_Window;
+   function Pick_Active return Boolean;
+   function Pick_Window_Size return Int;
+   procedure Pick_Matrix (Centre_X, Centre_Y : GL.Types.Size;
+                          Width, Height      : GL.Types.Size);
+   procedure Print_GL_Int3_Array
+     (Name : String; anArray : GL.Types.Ints.Vector3_Array);
+   function Rotor_To_GL_Matrix (R : Multivectors.Rotor)
                                  return GL.Types.Singles.Matrix4;
-    procedure Rotor_GL_Multiply (R : Multivectors.Rotor;
+   procedure Rotor_GL_Multiply (R         : Multivectors.Rotor;
                                 GL_Matrix : in out GL.Types.Singles.Matrix4);
-    function To_GL (V3 : Multivectors.Multivector) return GL.Types.Doubles.Vector3;
-    function To_GL (V3 : Multivectors.Multivector) return GL.Types.Singles.Vector3;
-    function To_GL (V3 : GA_Maths.Float_3D) return GL.Types.Singles.Vector3;
-    procedure Viewport_Coordinates (Pt_World : GA_Maths.Float_3D;
-                                    Model_View_Matrix,
-                                    Projection_Matrix : GL.Types.Singles.Matrix4;
-                                    Coords : out GL.Types.Singles.Vector2);
-
-private
-    type GL_Pick is record
-        Pick_Active      : Boolean := False;  --  set to true during picking
-        --  set to picking window (x, y, w, h) during picking
-        OpenGL_Pick      : Pick_Window := (others => 0.0);
-        --  Must be set correctly by caller of pick() to get correct distances returned
-        Frustum_Near     : Single := 1.0;
-        --  Must be set correctly by caller of pick() to get correct distances returned
-        Frustum_Far      : Single := 100.0;
-        --  not required for pick(), provided for completenes
-        FrustumWidth     : Single := 0.0;
-        --  not required for pick(), provided for completenes
-        Frustum_Height   : Single := 0.0;
-        Pick_Window_Size : Int := 4;
-    end record;
+   function To_GL (V3 : Multivectors.Multivector) return GL.Types.Doubles.Vector3;
+   function To_GL (V3 : Multivectors.Multivector) return GL.Types.Singles.Vector3;
+   function To_GL (V3 : GA_Maths.Float_3D) return GL.Types.Singles.Vector3;
+   procedure Viewport_Coordinates (Pt_World          : GA_Maths.Float_3D;
+                                   Model_View_Matrix,
+                                   Projection_Matrix : GL.Types.Singles.Matrix4;
+                                   Coords            : out GL.Types.Singles.Vector2);
 end GL_Util;
